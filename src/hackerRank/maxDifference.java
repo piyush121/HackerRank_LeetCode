15be6aef07e01e2919c331cac212990fcb4a9636
piyush121 <piyushchaudhary07@gmail.com>
2016-01-24 13:30:07 -0500

    Practice

--- src/hackerRank/maxDifference.java
+++ src/hackerRank/maxDifference.java
@@ -7,14 +7,13 @@ public class maxDifference {
 	static int max1Difference(int[] a) {
 		
 		int min = a[0];
-		int max_diff=a[1]-a[0];
+		int max_diff=-1;
 		for(int i=1;i<a.length;i++)
 		{
-			if(a[i]<min)
-				min=a[i];
-				
 			if(a[i]-min>max_diff)
 				max_diff=a[i]-min;
+			if(a[i]<min)
+				min=a[i];
 		}
 		
 		return max_diff;
@@ -35,7 +34,7 @@ public class maxDifference {
 	}
 	
 public static void main(String[] args) {
-	int [] arr={3,4,5,1,9};
-	System.out.println(maxdiff(arr));
+	int [] arr={9,5,3,1};
+	System.out.println(max1Difference(arr));
 }
 }
