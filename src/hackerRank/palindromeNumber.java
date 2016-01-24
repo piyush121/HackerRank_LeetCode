0309150a28ed50cb33de6dbba5422788183c2aa3
piyush121 <piyushchaudhary07@gmail.com>
2016-01-24 11:19:44 -0500

    Hello, It's me !

--- src/hackerRank/palindromeNumber.java
+++ src/hackerRank/palindromeNumber.java
@@ -3,28 +3,29 @@ package hackerRank;
 public class palindromeNumber {
 	
 	public static boolean isPalindrome(int x) {
-        int ten=10;
-        String str="";
-
-while(x/ten>=1)
-    {
-        str+=x%ten;
-        ten=ten*10;
-    }
-    
-    str+=(10*x)/ten;
-    System.out.println(str);
-    for(int i=0;i<str.length()/2;i++)
-        if(str.charAt(i)!=str.charAt(str.length()-1-i))
-            return false;
-    return true;
+       int p=1;
+       int r=1;
+       while(x/p>=10)
+    	 p*=10;
+       
+       while(p>r)
+       {
+    	   if((x/p)%10 == (x/r)%10)
+    	   { p=p/10;
+    	   	r=r*10;
+    	   }
+    	  else
+    	   	return false;
+       }
+        
+   return true;
         
         
     }
 	
 	
 	public static void main(String[] args) {
-		System.out.println(isPalindrome(202));
+		System.out.println(isPalindrome(10));
 	
 	}
 
