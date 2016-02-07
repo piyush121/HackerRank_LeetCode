7301c3cec8b57e6a44c7964afe52db428be672f2
piyush121 <piyushchaudhary07@gmail.com>
2016-02-06 20:24:39 -0500

    More Practice

---
+++ src/hackerRank/deckOfCards.java
@@ -0,0 +1,21 @@
+package hackerRank;
+
+class Card
+{
+	enum Suits{Spade,diamond,heart,club};
+	Suits suit;
+	int Rank;
+	Card(Suits s,int rank)
+	{
+		this.suit=s;
+		this.Rank=rank;
+	}
+}
+
+public class deckOfCards {
+	Card[] card;
+	
+	
+	
+
+}
