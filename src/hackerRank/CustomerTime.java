40f564fddda582c72cd24b7de936e6b0c77b13b1
piyush121 <piyushchaudhary07@gmail.com>
2016-02-08 14:29:51 -0500

    More Practice

--- src/hackerRank/CustomerTime.java
+++ src/hackerRank/CustomerTime.java
@@ -7,5 +7,16 @@ public class CustomerTime {
 	
 	HashMap<Integer, HashMap<String,Date>> cust=new HashMap<>();
 	
+	public boolean search(int custID, int prodID, Date date)
+	{	long time=0;;
+		if(cust.containsKey(custID))
+			if(cust.get(custID).containsKey(prodID))
+				if(cust.get(custID).get(prodID).before(date))
+					time = cust.get(custID).get(prodID).getTime() - date.getTime();
+		
+		if(time/(1000*60*60) >24 )
+			return true;
+		return false;
+	}
 
 }
