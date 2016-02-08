3dccdb6c41be420d7b5890bf60649fcd1b5f8f23
piyush121 <piyushchaudhary07@gmail.com>
2016-02-08 00:03:10 -0500

    More Practice

---
+++ src/hackerRank/ColumnName.java
@@ -0,0 +1,22 @@
+package hackerRank;
+
+public class ColumnName {
+	
+	public static int colIndex(String col)
+	{	int index=0;
+		int mul=0;
+		for(int i=col.length()-1;i>=0;i--)
+		{	
+			index  += (col.charAt(i)-64) * Math.pow(26, mul);
+			mul++;
+		}
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(colIndex("AAA"));
+		
+	}
+
+}
