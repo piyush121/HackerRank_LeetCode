39afa8a867268a0c2168aa642fe4c30f04d5507d
piyush121 <piyushchaudhary07@gmail.com>
2016-02-12 18:22:56 -0500

    Trying harder

--- src/hackerRank/InOrderIterative.java
+++ src/hackerRank/InOrderIterative.java
@@ -1,5 +1,36 @@
 package hackerRank;
 
+import java.util.Stack;
+
 public class InOrderIterative {
+	
+	public static void InTraversal(treeNode root)
+	{
+		
+		Stack <treeNode> mystack=new Stack<>();
+		while(root!=null)
+		{	
+			while(root.left!=null)
+				{
+					mystack.push(root);
+					root=root.left;
+				}
+			while(root.right!=null)
+				{
+					mystack.push(root);
+					root=root.right;
+
+				}
+
+			System.out.println(mystack.pop());
+			
+			
+		}
+		
+		System.out.println(root);
+		
+		
+		
+	}
 
 }
