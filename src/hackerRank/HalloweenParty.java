9e20244d479548a59c17bd15975de77f5ee5bd7a
piyush121 <piyushchaudhary07@gmail.com>
2016-02-12 01:45:07 -0500

    Trying harder

--- src/hackerRank/HalloweenParty.java
+++ src/hackerRank/HalloweenParty.java
@@ -2,12 +2,12 @@ package hackerRank;
 
 public class HalloweenParty {
 
-	static void max_Chocolates(int[] arr) {
+	static void max_Chocolates_not_Efficient(int[] arr) {
 	
 		for(int i=0;i<arr.length;i++)
 		{
-			int length=0;
-			int breadth=0;
+			long length=0;
+			long breadth=0;
 			int j=1;
 			while(j<=arr[i])
 			{
@@ -21,6 +21,14 @@ public class HalloweenParty {
 
 		}
 		
+	}
+	static void max_Chocolates_Efficient(int[] arr) {
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			long mid=arr[i]/2;
+			System.out.println(mid*arr[i]-1);
+		}
 	}
 	
 }
