dca0aeea3f42e9490bcb607868ed5eced9e0ab3f
piyush121 <piyushchaudhary07@gmail.com>
2016-02-12 18:57:38 -0500

    Trying harder

--- src/hackerRank/InOrderIterative.java
+++ src/hackerRank/InOrderIterative.java
@@ -8,29 +8,50 @@ public class InOrderIterative {
 	{
 		
 		Stack <treeNode> mystack=new Stack<>();
-		while(root!=null)
+		mystack.push(root);
+		while(mystack.peek().left!=null) //going left
+			mystack.push(mystack.peek().left);
+		while(!mystack.isEmpty())
 		{	
-			while(root.left!=null)
+			
+			treeNode node=mystack.pop();
+			System.out.println(node.data);
+			
+			if(node.right!=null)		//going right
 				{
-					mystack.push(root);
-					root=root.left;
+					mystack.push(node.right);
+					while(mystack.peek().left!=null) //going left
+						mystack.push(mystack.peek().left);
+				
 				}
-			while(root.right!=null)
-				{
-					mystack.push(root);
-					root=root.right;
 
 				}
 
-			System.out.println(mystack.pop());
 			
 			
 		}
 		
-		System.out.println(root);
-		
+public static void main(String[] args) {
+	
+	treeNode node1=new treeNode(4,null,null);
+	treeNode node2=new treeNode(5,null,null);
+	treeNode node3=new treeNode(2,null,null);
+	treeNode node4=new treeNode(1,null,null);
+	treeNode node5=new treeNode(3,null,null);
+	treeNode node6=new treeNode(3,null,null);
+
+	binaryTree mytree=new binaryTree();
+	mytree.add(node1);
+	mytree.add(node2);
+	mytree.add(node3);
+	mytree.add(node4);
+	mytree.add(node5);
+	mytree.add(node6);
+	System.out.println("In Order Traversal...");
+	InTraversal(mytree.root);
+}		
 		
 		
 	}
 
-}
+
