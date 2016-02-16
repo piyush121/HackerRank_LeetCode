daab2ae82e5c500751ae15f2f80eb02a884eefde
piyush121 <piyushchaudhary07@gmail.com>
2016-02-15 21:01:01 -0500

    Code Everyday

--- src/hackerRank/StringtoNum.java
+++ src/hackerRank/StringtoNum.java
@@ -10,7 +10,15 @@ public class StringtoNum {
 
 		System.out.println(num);
 	}
+	public static void strToNum1(String str)
+	{	
+		int num=0;
+		for(int i=str.length()-1;i>=0;i--)
+			num+=Math.pow(10, str.length()-1-i)* (str.charAt(i)-'0');
+
+		System.out.println(num);
+	}
 	public static void main(String[] args) {
-		strToNum("0435");
+		strToNum1("0435");
 	}
 }
