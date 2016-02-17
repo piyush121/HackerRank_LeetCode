61d67bdfc49ccb744f8f150063cf45d4196d253c
piyush121 <piyushchaudhary07@gmail.com>
2016-02-17 18:49:18 -0500

    Code Life

--- src/hackerRank/CheckBST.java
+++ src/hackerRank/CheckBST.java
@@ -1,8 +1,12 @@
 package hackerRank;
 
+import java.util.ArrayList;
+
 public class CheckBST {
 	static boolean left;
 	static boolean right;
+	static treeNode prev=null;
+	static ArrayList<Integer> arr=new ArrayList<>();
 	
 	public static boolean isBST(treeNode root,int min,int max)
 	{
@@ -13,6 +17,46 @@ public class CheckBST {
 		
 		return isBST(root.left,min,root.data)&&isBST(root.right,root.data,max);
 	}
+	
+	public static void  isBST1(treeNode root)
+	{
+			if(root==null)
+				return;
+			else
+			{
+			
+			isBST1(root.left);
+			arr.add(root.data);
+			isBST1(root.right);
+			}
+		
+		
+	}
+	public static boolean BSTUtil(treeNode root)
+	{
+		isBST1(root);
+		for(int i=0;i<arr.size()-1;i++)
+		{
+			if(arr.get(i)>arr.get(i+1))
+				return false;
+			
+		}
+		return true;
+	}
+	
+	public static boolean isBSTEff(treeNode root)  //Hard to understand !!!!!!!
+	{
+		if(root==null)
+			return true;
+		if(!isBSTEff(root.left))
+			return false;
+		
+		if(prev!=null && root.data<prev.data)
+			return false;
+		prev=root;
+		return isBSTEff(root.right);
+		
+	}	
 
 	 public static void main(String[] args) {
 	 		treeNode node1=new treeNode(4,null,null);
@@ -32,6 +76,8 @@ public class CheckBST {
 	 		//mytree.root.right=new treeNode(2,null,null);
 
 	 		System.out.println(isBST(mytree.root,Integer.MIN_VALUE,Integer.MAX_VALUE));
+	 		System.out.println(BSTUtil(mytree.root));
+	 		System.out.println(isBSTEff(mytree.root));
 	 		}
 
 }
