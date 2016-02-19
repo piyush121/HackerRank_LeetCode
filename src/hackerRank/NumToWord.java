bb0a1f87c2d23f660c52f3b7a8105f9096625c46
piyush121 <piyushchaudhary07@gmail.com>
2016-02-19 13:34:10 -0500

    Code Hard

---
+++ src/hackerRank/NumToWord.java
@@ -0,0 +1,69 @@
+package hackerRank;
+
+public class NumToWord { //Really a tedious problem.
+	String[] ones={"","one","two","three","four","five","six","seven","eight","nine"};
+	String[] teens={"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	String[] tens={"","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+	String[] bigs={"","thousand","million"};
+	
+	public String realWord(int num)
+	{
+		String str="";
+		int count=0;
+		while(num>0)
+		{	
+			
+			str=numToWord(num%1000)+" "+bigs[count]+" "+str;
+			num/=1000;
+			count++;
+		}
+		
+		return str;
+	}
+
+	public String numToWord(int num)
+	{
+		String str="";
+		int div=1;
+		while(num/div>=10)
+			div*=10;
+		while(div>=1)
+		{
+			if(div==100)
+				{
+					str+=ones[num/div]+" hundred";
+					num-=div*(num/div);
+				}
+
+			else if(div==10 && num/div>1)
+			{
+				str+=" "+tens[num/div-1];
+				num-=div*(num/div);
+			}
+			else if(div==10 && num/div==1)
+			{
+				num-=div*(num/div);
+				div/=10;
+				str+=" "+teens[num/div ];
+				break;
+			}
+			else
+				{
+					str+=" "+ones[num/div];
+					num-=div*(num/div);
+
+				}
+
+			div/=10;
+			
+		}
+		return str;
+		
+	}
+	
+	public static void main(String[] args) {
+		NumToWord obj=new NumToWord();
+		System.out.println(obj.realWord(123123));	
+
+}
+}
