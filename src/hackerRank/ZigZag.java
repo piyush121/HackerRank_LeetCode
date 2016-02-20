8ecaec71f8eb7396fc545a327b30749d6d77f463
piyush121 <piyushchaudhary07@gmail.com>
2016-02-20 12:48:31 -0500

    Some problems are very mathematical

--- src/hackerRank/ZigZag.java
+++ src/hackerRank/ZigZag.java
@@ -1,44 +1,39 @@
 package hackerRank;
 
 public class ZigZag {
-	
-	public static void print(String str,int rows)
-	{
-		boolean dir=true;
-		for(int i=0;i<rows;i++)
-		{	int j=i;
-			while(j<str.length())
-			{	
-			if(j%(rows-1)==0)
-					{
+
+	public static void print(String str, int rows) {
+		boolean dir = true;
+		for (int i = 0; i < rows; i++) {
+			int j = i;
+			int ptr = i;
+			dir = true;
+			while (j < str.length()) {
+				if (j % (rows - 1) == 0) {
+					System.out.print(str.charAt(j));
+					j += 2 * (rows - 1);
+				} else {
+					if (dir) {
+						// System.out.print(j);
 						System.out.print(str.charAt(j));
-						j+=2*(rows-1);
+						j += 2 * (rows - 1 - ptr);
+						dir = !dir;
+					} else {
+						// System.out.print(j);
+						System.out.print(str.charAt(j));
+						j += 2 * (ptr);
+						dir = !dir;
 					}
-			else
-			{
-				if(dir)	
-				{
-					System.out.print(str.charAt(j));
-					j+=2*(rows-1 -(j)%4);
-					dir=!dir;
+
 				}
-				else
-				{
-					System.out.print(str.charAt(j));
-					j+=2*((j)%4);
-					dir=!dir;
-				}				
-					
-			}
-				
+
 			}
 		}
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		print("paypalishiring",4);
+		print("paypalishiring", 4);
 	}
 
 }
