9a2061a1d8d0e19c0154daa0aa075f6a9a4c3907
piyush121 <piyushchaudhary07@gmail.com>
2016-02-22 14:12:31 -0500

    hustle everyday

--- src/hackerRank/LongestPalindromicSubsequence.java
+++ src/hackerRank/LongestPalindromicSubsequence.java
@@ -1,10 +1,19 @@
 package hackerRank;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import javax.print.attribute.HashAttributeSet;
+
 public class LongestPalindromicSubsequence {
 	
 	public static int maxLength(String str)
 	{
 		int[][] DP=new int[str.length()+1][str.length()+1];
+		StringBuilder strb=new StringBuilder();
+		TreeMap<Integer,Integer> parent=new TreeMap<>(Collections.reverseOrder());
+				
 		for(int i=0;i<=str.length();i++)
 			DP[i][i]=1;
 		
@@ -13,14 +22,31 @@ public class LongestPalindromicSubsequence {
 			{
 			
 				if(str.charAt(i-1)!=str.charAt(j-1))
-					DP[i][j]=Math.max(DP[i+1][j], DP[i][j-1]);
-				else if(str.charAt(i-1)==str.charAt(j-1) && j==i+1)
+					{
+						DP[i][j]=Math.max(DP[i+1][j], DP[i][j-1]);
+						
+					}
+				else if(str.charAt(i-1)==str.charAt(j-1) && j==2)
 					DP[i][j]=2;
 				else
-					DP[i][j]=DP[i+1][j-1]+2;
+					{
+						DP[i][j]=DP[i+1][j-1]+2;
+						parent.put(j-1,j-2);
+					}
 
 				
 			}
+		System.out.println(parent);
+			Integer key=parent.firstKey();
+			while(parent.containsKey(key)==true)
+			{
+				strb.append(str.charAt(key));
+				key=parent.get(key);
+			}
+			for(int i=strb.length()-1;i>=0;i--)
+				strb.append(strb.charAt(i));
+			System.out.println(strb.toString());
+
 		for(int i=0;i<=str.length();i++)
 			{
 			for(int j=0;j<=str.length();j++)
@@ -32,7 +58,7 @@ public class LongestPalindromicSubsequence {
 		
 	}
 	public static void main(String[] args) {
-		System.out.println(maxLength("eekee"));
+		System.out.println(maxLength("abba"));
 	}
 
 }
---
+++ src/hackerRank/LongestPlindromicSubstring.java
@@ -0,0 +1,51 @@
+package hackerRank;
+
+public class LongestPlindromicSubstring {
+	static int maxLen=1;
+	static String subString="";
+	
+	public static String LPS(String s)
+	{
+		 if(s.length()==1)
+		        return s;
+		 if(s.length()==0)
+		        return "";
+		 if(s.length()==2)
+		 {
+			 if(s.charAt(0)==s.charAt(1))
+				 return s;
+			 else 
+				 return String.valueOf(s.charAt(0));
+		 }
+		for(int i=0;i<s.length()-1;i++)
+		{
+			checkThis(s,i,i);
+			checkThis(s,i,i+1);
+		}
+		
+		return subString;
+	}
+	private static void checkThis(String s1,int index1,int index2)
+	{
+		int left=index1;
+		int right=index2;
+		while(left>=0 && right<s1.length()&&s1.charAt(left)==s1.charAt(right))
+		{
+			left--;
+			right++;
+		}
+		
+		if(right-left-1>=maxLen)
+			{
+				maxLen=right-left-1;
+				subString=s1.substring(left+1, right);
+			}
+		
+		
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(LPS("ccd"));
+	}
+
+}
