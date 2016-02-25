1a4e0941ee56ed8cc2d18deda3ec65438c10e79d
piyush121 <piyushchaudhary07@gmail.com>
2016-02-25 00:41:38 -0500

    hustle everyday

--- src/hackerRank/CountNodesPerLevel.java
+++ src/hackerRank/CountNodesPerLevel.java
@@ -9,48 +9,43 @@ import java.util.Queue;
  *
  */
 public class CountNodesPerLevel {
-	
-	public static void countNodesAtAllLevels(treeNode root){
-		if(root==null)
+
+	public static void countNodesAtAllLevels(treeNode root) {
+		if (root == null) // base case.
 			return;
-		Queue<treeNode> Q1=new LinkedList<>();
-		Queue<treeNode> Q2=new LinkedList<>();
-		ArrayList<Integer> countArray=new ArrayList<>();
-		int count=0;
+		Queue<treeNode> Q1 = new LinkedList<>();
+		Queue<treeNode> Q2 = new LinkedList<>();
+		ArrayList<Integer> countArray = new ArrayList<>();
+		int count = 0;
 		Q1.add(root);
-		while(!Q1.isEmpty())
-		{	
-			treeNode node= Q1.poll();
-			if(node.left!=null)
+		while (!Q1.isEmpty()) {
+			treeNode node = Q1.poll();
+			if (node.left != null)
 				Q2.add(node.left);
-			if(node.right!=null)
+			if (node.right != null)
 				Q2.add(node.right);
 			count++;
-			if(Q1.isEmpty())
-			{
+			if (Q1.isEmpty()) {
 				countArray.add(count);
-				count=0;
-				Q1=Q2;		//Flip queues when each level ends.
-				Q2=new LinkedList<>();
-				
-				
+				count = 0;
+				Q1 = Q2; // Flip queues when each level ends.
+				Q2 = new LinkedList<>();
+
 			}
 		}
 		System.out.println(countArray);
 	}
-	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		treeNode node1=new treeNode(4,null,null);
-		treeNode node2=new treeNode(5,null,null);
-		treeNode node3=new treeNode(2,null,null);
-		treeNode node4=new treeNode(1,null,null);
-		treeNode node5=new treeNode(3,null,null);
-		treeNode node6=new treeNode(3,null,null);
+		treeNode node1 = new treeNode(4, null, null);
+		treeNode node2 = new treeNode(5, null, null);
+		treeNode node3 = new treeNode(2, null, null);
+		treeNode node4 = new treeNode(1, null, null);
+		treeNode node5 = new treeNode(3, null, null);
+		treeNode node6 = new treeNode(3, null, null);
 
-		binaryTree mytree=new binaryTree();
+		binaryTree mytree = new binaryTree();
 		mytree.add(node1);
 		mytree.add(node2);
 		mytree.add(node3);
