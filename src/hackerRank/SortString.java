64d40a6044b1f6d996503acf9c57025dedc2506e
piyush121 <piyushchaudhary07@gmail.com>
2016-02-26 00:48:50 -0500

    Code and some more code

--- src/hackerRank/SortString.java
+++ src/hackerRank/SortString.java
@@ -2,10 +2,10 @@ package hackerRank;
 
 public class SortString {
 	
-	public static void sort(String str){
+	public static void sort(String str){ // Sort in case sensitive order.
 		char[] str1=str.toCharArray();
 		
-		for(int i=0;i<str.length();i++)
+		for(int i=0;i<str.length();i++) // Applying normal bubble sort.
 			for(int j=1;j<str.length();j++){
 				if(Character.compare(Character.toLowerCase(str1[j]),Character.toLowerCase(str1[j-1]))==0)
 					{if(str1[j]<str1[j-1]){
