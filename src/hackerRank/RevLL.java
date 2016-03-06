ed6e22a0239d19e9babf5ed5d6e5796d9a12bc48
piyush121 <piyushchaudhary07@gmail.com>
2016-03-06 00:07:35 -0500

    Practice makes perfect

--- src/hackerRank/RevLL.java
+++ src/hackerRank/RevLL.java
@@ -2,7 +2,7 @@ package hackerRank;
 
 public class RevLL {
 	
-	public static void reverse(ListNode node){
+	public static void reverse(ListNode node){ // amazing recursive approach.
 		if(node.next==null)
 			return;
 		
@@ -11,14 +11,27 @@ public class RevLL {
 		curr.next=node;
 		node.next=null;
 		
-	
+	}
+	public static void reverseitr(ListNode node) // iterative approach
+	{
+		ListNode prev=null;
+		ListNode curr=node;
+		ListNode next=curr.next;
+		while(curr!=null){
+			curr.next=prev;
+			prev=curr;
+			curr=next;
+			if(curr!=null) // boundary case when curr reaches end.
+				next=curr.next;
+			
+		}
 	}
 	public static void main(String[] args) {
 		ListNode l1=new ListNode(3);
 		l1.next=new ListNode(7);
 		l1.next.next=new ListNode(9);
 		ListNode ptr=l1.next.next;
-		reverse(l1);
+		reverseitr(l1);
 		
 		while(ptr!=null)
 			{
