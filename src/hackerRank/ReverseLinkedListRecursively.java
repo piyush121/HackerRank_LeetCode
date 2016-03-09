016ec82a503174bf36902ffb35469931af4f3ee0
piyush121 <piyushchaudhary07@gmail.com>
2016-03-09 08:27:58 -0500

    Practice makes perfect

--- src/hackerRank/ReverseLinkedListRecursively.java
+++ src/hackerRank/ReverseLinkedListRecursively.java
@@ -1,30 +1,32 @@
 package hackerRank;
 
 public class ReverseLinkedListRecursively {
-	
-	public static void reverse(ListNode head){
-		if(head.next==null)
+
+	/**
+	 * @author Piyush Chaudhary
+	 */
+	public static void reverse(ListNode head) {
+		if (head.next == null)
 			return;
-			
+
 		reverse(head.next);
-		ListNode curr=head.next;
-		curr.next=head;
-		head.next=null;
-		
+		ListNode curr = head.next;
+		curr.next = head;
+		head.next = null;
+
 	}
-	
+
 	public static void main(String[] args) {
-		ListNode l1=new ListNode(3);
-		l1.next=new ListNode(7);
-		l1.next.next=new ListNode(9);
-		ListNode ptr=l1.next.next;
+		ListNode l1 = new ListNode(3);
+		l1.next = new ListNode(7);
+		l1.next.next = new ListNode(9);
+		ListNode ptr = l1.next.next;
 		reverse(l1);
-		
-		while(ptr!=null)
-			{
-				System.out.println(ptr.val);
-				ptr=ptr.next;
-			}
+
+		while (ptr != null) {
+			System.out.println(ptr.val);
+			ptr = ptr.next;
+		}
 	}
 
 }
