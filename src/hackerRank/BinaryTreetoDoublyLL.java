e049cade38934ab69a262bd57567fe5158a2f33b
piyush121 <piyushchaudhary07@gmail.com>
2016-03-12 20:23:59 -0500

    Really Stuck on this one

--- src/hackerRank/BinaryTreetoDoublyLL.java
+++ src/hackerRank/BinaryTreetoDoublyLL.java
@@ -1,5 +1,24 @@
 package hackerRank;
 
 public class BinaryTreetoDoublyLL {
+	static treeNode last=null;
+	static treeNode head=null;
+	public static void binaryToLL(treeNode root){
+		if(root==null)
+			return;
+		binaryToLL(root.left);
+		if(last==null){
+			head=root;
+		}
+		else{
+			last.right=root;
+
+		}
+		root.left=last;
+		last=root;
+	
+		binaryToLL(root.right);
+	
+	}
 
 }
