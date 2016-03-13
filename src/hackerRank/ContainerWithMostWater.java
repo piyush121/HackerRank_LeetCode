788cda4ca9f72df52079a118a322fefa6a0fa853
piyush121 <piyushchaudhary07@gmail.com>
2016-03-13 17:26:09 -0400

    More Practice

---
+++ src/hackerRank/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package hackerRank;
+
+public class ContainerWithMostWater {
+	public int maxArea(int[] height) {
+		int maxArea = Integer.MIN_VALUE;
+		int i = 0;
+		int j = height.length - 1;
+
+		while (i < j) {
+			int area = Math.min(height[i], height[j]) * (j - i);
+			if (area > maxArea)
+				maxArea = area;
+			if (height[i] > height[j]) // Since the bottleneck is the smaller
+				j--;					// coordinate, so it's better to
+			else						// increment/decrement it int the hope
+				i++;					// of getting a bigger one.
+		}
+		return maxArea;
+	}
+}
