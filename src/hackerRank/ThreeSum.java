de658a186be25f35200c5392115d95283cc7e133
piyush121 <piyushchaudhary07@gmail.com>
2016-03-15 16:47:43 -0400

    Code Everyday

--- src/hackerRank/ThreeSum.java
+++ src/hackerRank/ThreeSum.java
@@ -1,5 +1,7 @@
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,12 +15,13 @@ public class ThreeSum {
 		if (nums.length == 0)
 			return new LinkedList<List<Integer>>();
 		LinkedList<List<Integer>> result = new LinkedList<>();
-
+		Arrays.sort(nums);
 		int start = 0;
 		int end = nums.length - 1;
 		int curr = start + 1;
-		while (start < end - 2) {
+		while (start <= end - 2) {
 			curr = start + 1;
+
 			while (curr < end) {
 				if (nums[start] + nums[curr] + nums[end] < 0)
 					curr++;
@@ -28,20 +31,32 @@ public class ThreeSum {
 					list.add(nums[curr]);
 					list.add(nums[end]);
 					result.add(list);
-					end--;
-				} else
-					end--;
+					while (nums[curr + 1] == nums[curr] && curr < end - 1)
+						curr++;
+					curr++;
 
+				} else {
+					while (nums[end - 1] == nums[end] && end > start + 1)
+						end--;
+					end--;
+				}
 			}
+			while (nums[start + 1] == nums[start] && start < end - 1)
+				start++;
 			start++;
+			end = nums.length - 1;
 
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
-		int[] arr = { -1, -1, 0, 1, 1, 2, 4 };
-		System.out.println(threeSum(arr));
+		int[] arr = { -1, -1, 0, 1, 1, 2, 2, 4 };
+		int[] arr1 = { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 };
+		int[] arr2 = { -5, -5, -4, -3, -2, 0, 2, 2 };
+
+		System.out.println(threeSum(arr2));
+
 	}
 
 }
