289b3356d20fe2ebb7f7ad84a71ab4a7a49a506d
piyush121 <piyushchaudhary07@gmail.com>
2016-03-15 19:12:38 -0400

    Code Everyday

--- src/hackerRank/ThreeSumClosest.java
+++ src/hackerRank/ThreeSumClosest.java
@@ -4,27 +4,33 @@ import java.util.Arrays;
 
 public class ThreeSumClosest {
 
-	public static int threeSumClosest(int[] nums, int target) {
-		if (nums.length < 3)
-			return 0;
+	public static int threeSumClosest(int[] nums, int target) { // O(n^2 +
+																// nlogn)
 		int closest = nums[0] + nums[1] + nums[2];
+		int leastDiff = Integer.MAX_VALUE;
 		Arrays.sort(nums);
 		for (int i = 0; i < nums.length - 2; i++) {
 			int start = i + 1;
 			int end = nums.length - 1;
 			while (start < end) {
 				int sum = nums[i] + nums[start] + nums[end];
+				int diff = Math.abs(sum - target);
 				if (sum == target)
 					return target;
 				if (sum < target) {
-					if (Math.abs(sum - target) < Math.abs(closest - target))
+					if (diff < leastDiff) // check how close this sum is from the target.
+					{ // should be as close to 0 as possible.
 						closest = nums[i] + nums[start] + nums[end];
+						leastDiff = diff;
+					}
 					start++;
 				}
 
 				else {
-					if (Math.abs(sum - target) < Math.abs(closest - target))
+					if (diff < leastDiff) {
 						closest = nums[i] + nums[start] + nums[end];
+						leastDiff = diff;
+					}
 					end--;
 				}
 			}
