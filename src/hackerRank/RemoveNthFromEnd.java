d71337ad718d2eee6ffc9b457de5e195f90e67ac
piyush121 <piyushchaudhary07@gmail.com>
2016-03-16 17:54:17 -0400

    Code Everyday

--- src/hackerRank/RemoveNthFromEnd.java
+++ src/hackerRank/RemoveNthFromEnd.java
@@ -23,5 +23,23 @@ public class RemoveNthFromEnd {// O(n) solution
 		}
 		return head.next;
 	}
+	
+	 public ListNode removeNthFromEnd1(ListNode head, int n) { //Another slightly  better solution.
+	        
+	      ListNode dummy=new ListNode(0);
+	      dummy.next=head;
+	      ListNode front=dummy;
+	      ListNode back=dummy;
+	      
+	      for(int i=0;i<=n;i++) {
+	          front=front.next;
+	      }
+	      while(front!=null) {
+	          front=front.next;
+	          back=back.next;
+	      }
+	    back.next=back.next.next;
+	    return dummy.next;
+	    }
 
 }
