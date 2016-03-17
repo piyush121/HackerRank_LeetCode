e5b0b22cb8b1ef8c83aa3a8eace82ee0d90bd500
piyush121 <piyushchaudhary07@gmail.com>
2016-03-17 15:29:28 -0400

    Code Everyday

--- src/hackerRank/ValidParenthesis.java
+++ src/hackerRank/ValidParenthesis.java
@@ -4,30 +4,29 @@ import java.util.Stack;
 
 public class ValidParenthesis {
 	public boolean isValid(String s) { // pretty fast.
-        if(s.length()==0)
-            return true;
-        
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++) {
-            char ch=s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{')
-                stack.push(ch);
-            else if(stack.size()==0)
-                return false;
-            else if (ch == ')' && stack.pop() != '(')
-                return false;
-            else if (ch == ']' && stack.pop() != '[')
-                return false;
-            else if (ch == '}' && stack.pop() != '{')
-                return false;
-            
-        }
-        
-        if(stack.size()!=0)
-            return false;
-        return true;
-        
-        
-    }
+		if (s.length() == 0)
+			return true;
+
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == '(' || ch == '[' || ch == '{')
+				stack.push(ch);
+			else if (stack.size() == 0)
+				return false;
+			else if (ch == ')' && stack.pop() != '(')
+				return false;
+			else if (ch == ']' && stack.pop() != '[')
+				return false;
+			else if (ch == '}' && stack.pop() != '{')
+				return false;
+
+		}
+
+		if (stack.size() != 0)
+			return false;
+		return true;
+
+	}
 
 }
