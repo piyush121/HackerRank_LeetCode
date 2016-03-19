4cf9dc2cf2e0ee68950dc5d4290f11e9b0594ac2
piyush121 <piyushchaudhary07@gmail.com>
2016-03-18 20:13:40 -0400

    Code Everyday

--- src/hackerRank/RemoveDuplicateElementsInPlace.java
+++ src/hackerRank/RemoveDuplicateElementsInPlace.java
@@ -6,15 +6,25 @@ public class RemoveDuplicateElementsInPlace {
 		if (nums.length == 0)
 			return 0;
 		int count = 1;
-		int j = 0;
+		int j = 1;
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] != nums[i - 1]) {
 				count++;
-				j++;
-				nums[j] = nums[i];
+				nums[j++] = nums[i];
 			}
 		}
 		return count;
 	}
+	
+	 public int removeDuplicates1(int[] nums) { // Efficient by a constant factor.
+	        if(nums.length==0)
+	            return 0;
+	        int j=1;
+	        for(int i=1;i<nums.length;i++) {
+	            if(nums[i]!=nums[i-1])
+	                nums[j++]=nums[i];
+	        }
+	        return j;
+	    }
 
 }
