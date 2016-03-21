fb92d948b44f83868b35673b6cbe4e90d46b8d3e
piyush121 <piyushchaudhary07@gmail.com>
2016-03-21 11:42:48 -0400

    Code Everyday

--- src/hackerRank/RemoveElementInPlace.java
+++ src/hackerRank/RemoveElementInPlace.java
@@ -1,12 +1,16 @@
 package hackerRank;
 
+/**
+ * @author Piyush Chaudhary
+ *
+ */
 public class RemoveElementInPlace { // LeetCode problem 27.
 
 	public int removeElement(int[] nums, int val) { // O(n) space.
 		if (nums.length == 0)
 			return 0;
 		int j = nums.length;
-		for (int i = nums.length - 1; i >= 0; i--) {
+		for (int i = nums.length - 1; i >= 0; i--) { // put every non-repeating in th front.
 			if (nums[i] == val) {
 				j--;
 				swap(nums, i, j);
