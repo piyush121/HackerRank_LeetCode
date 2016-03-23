e3219b8fb941dc59979ca3d0b757ac764057d8e6
piyush121 <piyushchaudhary07@gmail.com>
2016-03-22 20:47:12 -0400

    Code Everyday

--- src/hackerRank/Heap.java
+++ src/hackerRank/Heap.java
@@ -3,12 +3,11 @@ package hackerRank;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Heap {
-	static PriorityQueue<Integer> queue=new PriorityQueue<>(20,Collections.reverseOrder());
-	public static void topThree(int[] arr)
-	{
-		for(int i=0;i<arr.length;i++)
-		{
+public class Heap { // Minimum k elements.
+	static PriorityQueue<Integer> queue = new PriorityQueue<>(20, Collections.reverseOrder());
+
+	public static void topThree(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
 			queue.add(arr[i]);
 		}
 		System.out.println(queue.remove());
@@ -16,9 +15,10 @@ public class Heap {
 		System.out.println(queue.remove());
 		System.out.println(queue.remove());
 	}
+
 	public static void main(String[] args) {
-		int[] arr={5,3,7,4,8,1,9,2};
+		int[] arr = { 5, 3, 7, 4, 8, 1, 9, 2 };
 		topThree(arr);
 	}
-	
+
 }
