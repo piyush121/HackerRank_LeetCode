a3cdbd7533084c4d01362e2138e7de9b12bf7edd
piyush121 <piyushchaudhary07@gmail.com>
2016-03-23 14:49:02 -0400

    Code Everyday

--- src/hackerRank/BinaryTreetoDoublyLL.java
+++ src/hackerRank/BinaryTreetoDoublyLL.java
@@ -5,7 +5,8 @@ public class BinaryTreetoDoublyLL {
 									// in-order fashion.
 	static treeNode head = null; // head pointer in LL.
 
-	public static void binaryToDLL(treeNode root) { // pretty hard to understand.
+	public static void binaryToDLL(treeNode root) { // pretty hard to
+													// understand.
 		if (root == null)
 			return;
 		binaryToDLL(root.left);
@@ -19,16 +20,30 @@ public class BinaryTreetoDoublyLL {
 		binaryToDLL(root.right);
 
 	}
-	
-	public static void binarytoCLL(treeNode node) { // A different implementation.
+
+	public static treeNode binarytoCLL(treeNode node) { // A different
+														// implementation.
 		binarytoCLL(node.left);
 		binarytoCLL(node.right);
-		
+		node = concat(node, node.left);
+		node = concat(node, node.right);
+
+		return node;
+
 	}
-	
-	public static treeNode concat(treeNode a,treeNode b) {
-		
+
+	public static treeNode concat(treeNode a, treeNode b) {
+		if (a == null)
+			return b;
+		if (b == null)
+			return a;
+		treeNode aEnd = concat(a, a.left);
+		treeNode bEnd = concat(b, b.left);
+		a.left = bEnd;
+		aEnd.right = b;
+		b.left = aEnd;
+
+		return a;
 	}
-	
 
 }
