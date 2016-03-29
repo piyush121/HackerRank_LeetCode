e1e7b735fa1354679c0dd5cc895af3a11f9cb058
piyush121 <piyushchaudhary07@gmail.com>
2016-03-29 00:20:26 -0400

    Code Everyday

--- src/hackerRank/CoinChange.java
+++ src/hackerRank/CoinChange.java
@@ -2,8 +2,21 @@ package hackerRank;
 
 public class CoinChange {
 	
-	public static minCoinsRecursive(int[] coins , int sum) {
+	public static int minCoinsRecursive(int[] coins , int sum) {
+		if(sum == 0) return 0;
+		if(sum < 0) return Integer.MAX_VALUE;
+		int min=0;
+		for(int coin : coins) {
+			min = 1 + minCoinsRecursive(coins,sum - coin);
+			
+		}
 		
+		return Math.min(sum, min);
+	}
+	
+	public static void main(String[] args) {
+		int[] coins = {25,10,5,1};
+		System.out.println(minCoinsRecursive(coins, 32));
 	}
 
 }
