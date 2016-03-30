0816f46fee4f71f36bae6757e8479639e9d37429
piyush121 <piyushchaudhary07@gmail.com>
2016-03-30 10:23:19 -0400

    Code Everyday

--- src/hackerRank/CoinChange.java
+++ src/hackerRank/CoinChange.java
@@ -1,5 +1,7 @@
 package hackerRank;
 
+import java.util.Arrays;
+
 public class CoinChange {
 
 	public static int minCoinsRecursive(int[] coins, int sum) { // Recursive and highly inefficient.
@@ -10,21 +12,38 @@ public class CoinChange {
 		for (int coin : coins) {
 			if(sum - coin >= 0) {
 				int count = 1 + minCoinsRecursive(coins, sum - coin); // recursive call on every combination.
-			if (count < minCount)
-				minCount = count;
+				if (count < minCount)
+					minCount = count;
 			}
 		}
 
 		return Math.min(sum, minCount);
 	}
 	
-	public  static int minCoinsDP(int[] coins, int sum) {
-		
+	public  static int minCoinsDP(int[] coins, int sum, int[] cache) {
+		int minCount = sum;
+		for (int coin : coins) {
+			int count;
+			if (sum - coin >= 0) {
+				if (cache[sum] < 0)
+					count = 1 + minCoinsDP(coins, sum - coin, cache);
+				else
+					count = cache[sum];
+				if (count < minCount)
+					minCount = count;
+
+			}
+		}
+		return minCount;
 	}
 
 	public static void main(String[] args) {
 		int[] coins = { 25, 10, 5, 1 };
-		System.out.println(minCoinsRecursive(coins, 16));
+		System.out.println(minCoinsRecursive(coins, 32));
+		int[] cache = new int[33];
+		Arrays.fill(cache, -1);
+		cache[0]=0;
+		System.out.println(minCoinsDP(coins, 32, cache));
 	}
 
 }
