22b841beee6855085bf86ff9f167b4737cd469bc
piyush121 <piyushchaudhary07@gmail.com>
2016-04-04 17:25:42 -0400

    Efficiency vs Sloppyness

--- src/hackerRank/RangeSearch.java
+++ src/hackerRank/RangeSearch.java
@@ -1,36 +1,50 @@
 package hackerRank;
 
+import java.util.Arrays;
+
 public class RangeSearch {
-	 public int[] searchRange(int[] nums, int target) { //O(log n) solution.
-		 
-		 int index = binarySearch(nums, target, 0, nums.length - 1);
-		 int lower = lhBound(nums, target, 0, index - 1, index); //Find lower index
-		 int higher = lhBound(nums, target, index + 1,  nums.length - 1, index); // find upper indexx
-		 
-		 return new int[]{lower,higher};
-	        
-	    }
-	private int binarySearch(int[] arr,int val, int start, int end) {
-		if(start < end)
+	public static int[] searchRange(int[] nums, int target) { // O(log n) solution.
+		int lower = -1;
+		int higher = -1;
+		int index = binarySearch(nums, target, 0, nums.length - 1);
+		if (index != -1) {
+			
+			lower = lhBound(nums, target, 0, index - 1, index); // Find lower index
+																 
+			higher = lhBound(nums, target, index + 1, nums.length - 1, index); // find upper index
+		}
+
+		return new int[] { lower, higher };
+
+	}
+
+	private static int binarySearch(int[] arr, int val, int start, int end) {
+		if (start > end)
 			return -1;
-		int mid = (start + end)/2;
-		if(arr[mid] == val)
+		int mid = (start + end) / 2;
+		if (arr[mid] == val)
 			return mid;
-		else if(arr[mid] < val)
+		else if (arr[mid] < val)
 			return binarySearch(arr, val, mid + 1, end);
 		else
 			return binarySearch(arr, val, start, mid - 1);
-		
+
 	}
-	
+
 	private static int lhBound(int[] arr, int val, int start, int end, int index) {
-		if(start < end)
+		if (start > end)
 			return index;
-		int mid = (start + end)/2;
-		if(arr[mid] == val)
+		int mid = (start + end) / 2;
+		if (arr[mid] == val)
 			return lhBound(arr, val, start, mid - 1, mid);
 		else
 			return lhBound(arr, val, mid + 1, end, index);
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {5, 7, 7, 8, 8, 10};
+		int[] nums1 = {1,2,3,3,3,3,4,5,9};
+		System.out.println(Arrays.toString(searchRange(nums1, 3)));
 	}
 
 }
