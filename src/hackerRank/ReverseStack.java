2a018f7a5d51648e0cc656dc5c302854571187fd
piyush121 <piyushchaudhary07@gmail.com>
2016-04-06 10:45:39 -0400

    Code...

--- src/hackerRank/ReverseStack.java
+++ src/hackerRank/ReverseStack.java
@@ -2,11 +2,9 @@ package hackerRank;
 
 import java.util.Stack;
 
-import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+public class ReverseStack { //Recursive implementation without using any data structure.
 
-public class ReverseStack {
-
-	public static void reverse(Stack<Integer> stack) {
+	public static void reverse(Stack<Integer> stack) { //O (n^2) time O (n) space.
 		if (stack.isEmpty())
 			return;
 		int temp = stack.pop();
@@ -14,11 +12,25 @@ public class ReverseStack {
 		insertAtBottom(stack, temp);
 	}
 
-	public static void insertAtBottom(Stack<Integer> stack, int val) {
-		if (stack.isEmpty())
+	public static void insertAtBottom(Stack<Integer> stack, int val) { //O(n) time. O(n) space.
+		if (stack.isEmpty()) {
 			stack.push(val);
+			return;
+		}
 		int temp = stack.pop();
 		insertAtBottom(stack, val);
 		stack.push(temp);
+	}
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		System.out.println(stack);
+
+		reverse(stack);
+		System.out.println(stack);
 	}
 }
