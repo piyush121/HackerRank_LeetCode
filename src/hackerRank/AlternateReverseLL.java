5b4d81ce24f962797af8da5f82a447f137a61fe8
piyush121 <piyushchaudhary07@gmail.com>
2016-04-08 10:46:09 -0400

    Morning commit

--- src/hackerRank/AlternateReverseLL.java
+++ src/hackerRank/AlternateReverseLL.java
@@ -8,9 +8,9 @@ package hackerRank;
  */
 public class AlternateReverseLL {
 
-	public static void reverseLL(LinkNode node) { // O(n)
+	public static LinkNode reverseLL(LinkNode node) { // O(n)
 		if (node == null || node.next == null)
-			return;
+			return node;
 		LinkNode prev = null;
 		LinkNode curr = node;
 		LinkNode next = node.next;
@@ -21,6 +21,50 @@ public class AlternateReverseLL {
 			if (next != null)
 				next = next.next;
 		}
+		return prev;
+	}
+
+	public static void alternateReverseLL(LinkNode node) {
+		int size = 0;
+		LinkNode ptr = node;
+		while (ptr != null) { // Find size of LL. O(n)
+			System.out.print(ptr.data + " -> ");
+			ptr = ptr.next;
+			size++;
+		}
+		int mid = size / 2;
+		LinkNode first = node;
+		LinkNode second = null;
+		ptr = first;
+
+		while (mid > 0) {
+			ptr = ptr.next;
+			mid--;
+		}
+		LinkNode temp = ptr.next;
+		ptr.next = null;
+		second = temp;
+		second = reverseLL(second); // O(n)
+		mergeList(first, second);
+	}
+
+	public static void mergeList(LinkNode first, LinkNode second) {
+		if (first == null || second == null)
+			return;
+		LinkNode temp1 = first.next;
+		LinkNode temp2 = second.next;
+
+		while (first != null && second != null) {
+			first.next = second;
+			second.next = temp1;
+			first = temp1;
+			second = temp2;
+			if (temp1 != null)
+				temp1 = temp1.next;
+			if (temp2 != null)
+				temp2 = temp2.next;
+
+		}
 	}
 
 	public static void main(String[] args) {
@@ -30,7 +74,6 @@ public class AlternateReverseLL {
 		node.next.next.next = new LinkNode(4);
 		LinkNode last = new LinkNode(5);
 		node.next.next.next.next = last;
-
 		reverseLL(node);
 		LinkNode ptr = last;
 		while (ptr != null) {
