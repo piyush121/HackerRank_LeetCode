d95dd474d168fdf291215a5a425d49eae51ed806
piyush121 <piyushchaudhary07@gmail.com>
2016-04-18 10:47:57 -0400

    Yet another Commit

--- src/hackerRank/ArrayPermutation.java
+++ src/hackerRank/ArrayPermutation.java
@@ -1,32 +1,40 @@
 package hackerRank;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ArrayPermutation {
 	 public static  List<List<Integer>> permute(int[] nums) {
-	        return permute (nums, -1, new LinkedList<Integer>());
+		 List<List<Integer>> result = new LinkedList<>();
+	         permute (nums, 0, result);
+	         return result;
 	    }
 	 
-	 public static List<List<Integer>> permute(int[] nums,int skipIndex, List<Integer> perms) {
+	 public static void permute(int[] nums, int start, List<List<Integer>> result) {
 		 
-		 List<List<Integer>> result = new LinkedList<>();
-
-		 if(perms.size() == nums.length) {
-			 result.add(perms);
-			 return result;
+		 if( start == nums.length -1) {
+			 List<Integer> list = new LinkedList<Integer>();
+		        for(int i=0;i<nums.length;i++){
+		            list.add(nums[i]);
+		        }
+		        result.add(list);
+		        return;
 		 }
 		 
-		 for(int i = 0; i < nums.length; i++) {
-			 if(i == skipIndex)
-				 continue;
-			 perms.add(nums[i]);
-			 result = permute(nums,i, perms );
-			 if( i != nums.length - 1)
-				 result.add(new LinkedList<>());
-		 }	
+		 for(int i = start; i < nums.length; i++) {
+			 swap(nums, start, i);
+			 permute(nums, start + 1, result );
+			 swap(nums, start, i);
+		 }
 		 
-		 return result;
+		 
+	 }
+	 
+	 public static void swap (int[] nums, int i, int j) {
+		 int temp = nums[i];
+         nums[i] = nums[j];
+         nums[j] = temp;
 	 }
 	 
 	 public static void main(String[] args) {
