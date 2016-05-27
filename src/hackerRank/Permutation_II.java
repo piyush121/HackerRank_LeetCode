58c7f65cb3c2e545fdf91798206a23467ce77e12
piyush121 <piyushchaudhary07@gmail.com>
2016-05-27 16:16:00 -0400

    back to leetcode!

---
+++ src/hackerRank/Permutation_II.java
@@ -0,0 +1,25 @@
+package hackerRank;
+
+import java.util.List;
+
+/**
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+ *	For example,
+ *	[1,1,2] have the following unique permutations:
+ *	[1,1,2], [1,2,1], and [2,1,1].
+ */
+
+
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class Permutation_II {
+	
+public List<List<Integer>> permuteUnique(int[] nums) {
+	
+	
+        
+    }
+
+}
