8b007def708b4e1a5bba1d20888f70a8f19401e2
piyush121 <piyushchaudhary07@gmail.com>
2016-05-27 20:25:23 -0400

    Code life

--- src/hackerRank/Permutation_II.java
+++ src/hackerRank/Permutation_II.java
@@ -1,5 +1,8 @@
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,10 +19,48 @@ import java.util.List;
  */
 public class Permutation_II {
 	
-public List<List<Integer>> permuteUnique(int[] nums) {
+public static List<List<Integer>> permuteUnique(int[] nums) {
 	
-	
-        
+	List<List<Integer>> permutations = permute(nums, 0, new LinkedList<List<Integer>>());
+    return permutations;
     }
+
+private static List<List<Integer>> permute(int[] arr, int start, List<List<Integer>> result){
+	if(start >= arr.length) {
+		List<Integer> temp = new LinkedList<>();
+
+		for (int val: arr) {
+			temp.add(val);
+		}
+		result.add(temp);
+		return result;
+	}
+	
+	else
+	{
+		for(int i = start; i < arr.length; i++) {
+			swap(arr, i, start);
+			permute(arr, start + 1, result);
+			swap(arr, i, start);
+			
+		}
+		
+	}
+	
+	return result;
+}
+
+private static void swap (int[] arr, int i, int j) {
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+	
+	
+}
+
+public static void main(String[] args) {
+	int[] array = {1,2,3};
+	System.out.println(permuteUnique(array));
+}
 
 }
