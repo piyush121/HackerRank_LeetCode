490f5081af51502163ac77d2ee20508200ff17b8
piyush121 <piyushchaudhary07@gmail.com>
2016-06-06 00:58:19 -0400

    Codeeeee

---
+++ src/hackerRank/Rotate_Image.java
@@ -0,0 +1,27 @@
+package hackerRank;
+
+/*
+ * You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+ * 
+ */
+
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class Rotate_Image {
+	
+	 public void rotate(int[][] matrix) {
+	        
+	    }
+	 
+	 public static void main(String[] args) {
+		
+	}
+
+}
