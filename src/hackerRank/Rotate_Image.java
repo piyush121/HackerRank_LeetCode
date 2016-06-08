2826d121f954c98abc0685c75864ae2aad3b188f
piyush121 <piyushchaudhary07@gmail.com>
2016-06-07 23:13:45 -0400

    :|

--- src/hackerRank/Rotate_Image.java
+++ src/hackerRank/Rotate_Image.java
@@ -1,5 +1,7 @@
 package hackerRank;
 
+import java.util.Arrays;
+
 /*
  * You are given an n x n 2D matrix representing an image.
 
@@ -16,19 +18,27 @@ Could you do this in-place?
  */
 public class Rotate_Image {
 	
-	 public void rotate(int[][] matrix) {
+	 public static void rotate(int[][] matrix) {
 		 int len = matrix.length;
 		 int result[][] = new int[len][len];
 
 	        for (int i = 0; i < len; i++)
 	        	for(int j = 0 ; j < len; j++) {
-	        		result[i][j] = matrix[j][len - i];
-	        		
+	        		result[j][len - i - 1] = matrix[i][j];
 	        	}
+	     matrix = result;
 	    }
 	 
 	 public static void main(String[] args) {
+		int test[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 		
+		rotate(test);
+		for (int i = 0; i < 4; i++){
+        	for(int j = 0 ; j < 4; j++) 
+        		System.out.print(test[i][j]+"\t");
+    		System.out.println();
+
+		}
 	}
 
 }
