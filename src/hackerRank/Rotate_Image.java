17e6165b374ab6ddc4b26888cf02964de5e38d3d
piyush121 <piyushchaudhary07@gmail.com>
2016-06-08 00:08:12 -0400

    Code life !

--- src/hackerRank/Rotate_Image.java
+++ src/hackerRank/Rotate_Image.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 Rotate the image by 90 degrees (clockwise).
 
 Follow up:
-Could you do this in-place?
+Could you do this in-place? :O
  * 
  */
 
@@ -24,17 +24,38 @@ public class Rotate_Image {
 
 	        for (int i = 0; i < len; i++)
 	        	for(int j = 0 ; j < len; j++) {
-	        		result[j][len - i - 1] = matrix[i][j];
+	        		result[j][len - i - 1] = matrix[i][j]; // The only medium hard part for this ques.
 	        	}
-	     matrix = result;
+	        for(int i = 0; i< len ; i++) {
+	        	matrix[i] = result[i];
+	        }
+	        		
+	    }
+	 public static void rotateInPlace(int[][] matrix) { // Amazing solution !!! in place O(1) space O(n^2) time.
+		 int len = matrix.length;
+		 int[] temp = null;
+		 for(int i=0 ; i < len/2 ; i++) { //reverse the array from top to bottom.
+			 temp = matrix[i];
+			 matrix[i] = matrix[len - 1 - i] ;
+			 matrix[len - 1 - i] = temp;
+		 }
+		 
+		 	int var = 0;
+	        for (int i = 0; i < len; i++) {
+	        	for(int j = i + 1 ; j < len; j++) { // j = i + 1 => imp. concept !
+	        	 var = matrix[i][j] ;
+	   			 matrix[i][j] = matrix[j][i] ;
+	   			 matrix[j][i] = var;	  
+	   			 }
+	        }
 	    }
 	 
 	 public static void main(String[] args) {
-		int test[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		int test[][] = {{1,2},{3,4}};
 		
-		rotate(test);
-		for (int i = 0; i < 4; i++){
-        	for(int j = 0 ; j < 4; j++) 
+		rotateInPlace(test);
+		for (int i = 0; i < 2; i++){
+        	for(int j = 0 ; j < 2; j++) 
         		System.out.print(test[i][j]+"\t");
     		System.out.println();
 
