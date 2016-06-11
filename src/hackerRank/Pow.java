f485cfd4865c057c7b0d5753ddd14fe05f6537e3
piyush121 <piyushchaudhary07@gmail.com>
2016-06-11 16:14:38 -0400

    Recursion is hard !

--- src/hackerRank/Pow.java
+++ src/hackerRank/Pow.java
@@ -9,18 +9,23 @@ package hackerRank;
  *
  */
 public class Pow {
-	
-	 public static double myPow(double x, int n) {
-		 
-		if( n % 2 == 0) {
-			return myPow(x*x, n/2);
+
+	public static double myPow(double x, int n) {
+		if (n == 0)
+			return 1;
+		else if (n % 2 == 0) {
+			return myPow(x * x, n / 2);
 		}
-		else
-			return myPow(x*x, n - 1);
-		
-	    }
-	 public static void main(String[] args) {
-		System.out.println(myPow(3.89707, 2));
+		else if (n < 0) {
+			n = -n;
+			x = 1 / x;
+		} 
+		return x * myPow(x, n - 1);
+
+	}
+
+	public static void main(String[] ars) {
+		System.out.println(myPow(8.88, 3));
 	}
 
 }
