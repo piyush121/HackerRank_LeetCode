3ae51d2967db5f8edf7c4bf2a57d722e64bc86e9
piyush121 <piyushchaudhary07@gmail.com>
2016-06-11 16:00:07 -0400

    Power

--- src/hackerRank/Pow.java
+++ src/hackerRank/Pow.java
@@ -11,42 +11,13 @@ package hackerRank;
 public class Pow {
 	
 	 public static double myPow(double x, int n) {
-		 double num = x;
-		 if( n == 0)
-			 return 1;
-		 else if (n > 0) {
-			 while(n > 0) {
-				 if(n != 1) {
-					x *= x;
-				 	n /= 2;
-				 }
-				 
-				 if(n == 1) {
-					 x *= num;
-					 --n;
-				 }
-					 
-				 }
-			 return x;
-		 }
-		 else {
-			 x = 1.0 / x;
-			 num = x;
-			 n = -n;
-			 while(n > 0) {
-				 if(n != 1) {
-					x *= x;
-				 	n /= 2;
-				 }
-				 
-				 if(n == 1) {
-					 x *= num;
-					 --n;
-				 }
-			 }
-				 return x;
-		 }
-	        
+		 
+		if( n % 2 == 0) {
+			return myPow(x*x, n/2);
+		}
+		else
+			return myPow(x*x, n - 1);
+		
 	    }
 	 public static void main(String[] args) {
 		System.out.println(myPow(3.89707, 2));
