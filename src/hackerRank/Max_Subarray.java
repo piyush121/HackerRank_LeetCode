e698dff660ddb1791f9c0a5b5e9be52bfcb9082a
piyush121 <piyushchaudhary07@gmail.com>
2016-06-12 11:53:27 -0400

    Code and more code...

---
+++ src/hackerRank/Max_Subarray.java
@@ -0,0 +1,31 @@
+package hackerRank;
+/*
+ * 
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+ */
+
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class Max_Subarray {
+	public int maxSubArray(int[] nums) {
+		int max = nums[0];
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (sum > max) {
+				max = sum;
+			}
+			if (sum > 0)
+				continue;
+			else {
+				sum = 0;
+			}
+		}
+		return max;
+	}
+}
