df48f7eee736cb7b0b2a7b85c51b6575137bcafc
piyush121 <piyushchaudhary07@gmail.com>
2016-06-13 22:09:55 -0400

    little things can fail many test cases.

--- src/hackerRank/SpiralMtrx.java
+++ src/hackerRank/SpiralMtrx.java
@@ -6,27 +6,28 @@ import java.util.List;
 public class SpiralMtrx {
 	
 	 public static List<Integer> spiralOrder(int[][] matrix) {
-	        
+	        List<Integer> result = new ArrayList<>();
+	        if( matrix.length == 0)
+	        	return result;
 	        int colStart = 0;
 	        int colEnd = matrix[0].length - 1;
 	        int rowStart = 0;
 	        int rowEnd = matrix.length - 1;
-	        List<Integer> result = new ArrayList<>();
 	        
 	        while (colStart <= colEnd && rowStart <=rowEnd) {
-	        for(int i = colStart; i <= colEnd; i++)
+	        for(int i = colStart; i <= colEnd && rowStart <= rowEnd; i++) // You gotta check both the conditions everytime !
 	        	result.add(matrix[rowStart][i]);
         	rowStart++;
 
-	        for(int i = rowStart; i <= rowEnd; i++)
+	        for(int i = rowStart; i <= rowEnd && colStart <= colEnd; i++)
 	        	result.add(matrix[i][colEnd]);
         	colEnd--;
 
-	        for(int i = colEnd; i >= colStart; i--)
+	        for(int i = colEnd; i >= colStart && rowStart <= rowEnd; i--)
 	        	result.add(matrix[rowEnd][i]);
         	rowEnd--;
 
-	        for(int i = rowEnd; i >= rowStart; i--)
+	        for(int i = rowEnd; i >= rowStart && colStart <= colEnd; i--)
 	        	result.add(matrix[i][colStart]);
 	        colStart++;
 	        }
@@ -36,7 +37,7 @@ public class SpiralMtrx {
 	 
 	 public static void main(String[] args){
 			
-			int[][] array={{1,2,3},{4,5,6},{7,8,9}};
+			int[][] array={{2,3}};
 			System.out.println(spiralOrder(array)); //send input correctly.
 		}
 
