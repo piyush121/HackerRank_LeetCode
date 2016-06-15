7c8d60c188a51e9d3b7b887e0102b344d41c0af8
piyush121 <piyushchaudhary07@gmail.com>
2016-06-14 22:41:36 -0400

    Leet...

---
+++ src/hackerRank/Jump_Game.java
@@ -0,0 +1,23 @@
+package hackerRank;
+
+/*
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+ */
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class Jump_Game {
+	 public static boolean canJump(int[] nums) {
+	        
+	    }
+}
