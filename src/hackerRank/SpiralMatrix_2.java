fa49da174eb480ca8667901d49c4f7d70c31ec1b
piyush121 <piyushchaudhary07@gmail.com>
2016-06-17 00:29:14 -0400

    Code Everyday!

--- src/hackerRank/SpiralMatrix_2.java
+++ src/hackerRank/SpiralMatrix_2.java
@@ -20,7 +20,43 @@ You should return the following matrix:
  */
 public class SpiralMatrix_2 {
 	public static int[][] generateMatrix(int n) {
-        
+		int[][] result = new int[n][n];
+		int colStart = 0;
+		int rowStart = 0;
+		int colEnd = n - 1;
+		int rowEnd = n - 1;
+		int count = 1;
+
+		while (colStart <= colEnd) {
+
+			for (int i = colStart; i <= colEnd; i++) {
+				result[rowStart][i] = count;
+				count++;
+			}
+			rowStart++;
+
+			for (int j = rowStart; j <= rowEnd; j++) {
+				result[j][colEnd] = count;
+				count++;
+			}
+			colEnd--;
+
+			for (int i = colEnd; i >= colStart; i--) {
+				result[rowEnd][i] = count;
+				count++;
+			}
+			rowEnd--;
+
+			for (int j = rowEnd; j >= rowStart; j--) {
+				result[colStart][j] = count;
+				count++;
+			}
+			colStart++;
+
+		}
+
+		return result;
+
 	}
 
 }
