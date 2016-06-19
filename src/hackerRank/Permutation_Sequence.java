891b31cf906d12eaa3ae86d398ffc73a22be8b41
piyush121 <piyushchaudhary07@gmail.com>
2016-06-19 00:29:43 -0400

    New day new problem !

--- src/hackerRank/Permutation_Sequence.java
+++ src/hackerRank/Permutation_Sequence.java
@@ -1,5 +1,8 @@
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
  * 
  * The set [1,2,3,…,n] contains a total of n! unique permutations.
@@ -22,28 +25,58 @@ Note: Given n will be between 1 and 9 inclusive.
  *
  */
 public class Permutation_Sequence {
-	static int level = 0;
-	public static String getPermutation(int n, int k) {
+	static int level = 1;
+	static String result = "";
+
+	public static String getPermutation(int n, int k) { // Slow and pathetic solution. Damn ! O(n!)
 		StringBuilder strb = new StringBuilder();
 		for (int i = 1; i <= n; i++) {
 			strb.append(i);
 		}
-		String result = permuteHelper(strb.toString(),strb.toString(),"", k, level);
-		
+		permuteHelper("", strb.toString(), k);
+
 		return result;
 	}
 
-	private static String permuteHelper(String str, String prefix, String suffix, int k, int level) {
-		if( prefix == "" ) {
-			if( k == level)
-				return suffix;
-			else
-				level++;
+	private static void permuteHelper( String prefix, String suffix, int k) {
+		if (suffix.length() == 0) {
+			if (k == level)
+				result = prefix;
+				
+			level++;
+			return;
+		}
+		for (int i = 0; i < suffix.length(); i++) {
+			permuteHelper( prefix + suffix.charAt(i), suffix.substring(0, i) + suffix.substring(i+1), k);
+		}
+	}
+	public static String getPermutation1(int n, int k) { //Optimized one !So many smart people are present on this earth for helping us.
+	
+		StringBuilder result = new StringBuilder();
+		int[] factorials = new int[n + 1];
+		factorials[0] = 1;
+		for(int i = 1; i <= n ; i++) { // calculating the factorials up to the max possible permutations.
+			factorials[i] = factorials[i - 1] * i;
 		}
 		
-		for( int i = 0; i < str.length(); i++ ) {
-			permuteHelper(str, str.substring(0,i),str.substring(i),k, level);
+		ArrayList<Integer> nums =  new ArrayList<>();
+		for(int i = 1; i <= n; i ++) {
+			nums.add(i);
 		}
+		k--;
+		for(int i = 0; i < n; i++ ) { //trying to find pattern here.
+			int fact = factorials[n - i];
+			int magicNumber = fact / (n - i);
+			int index = k / magicNumber;
+			result.append(String.valueOf(nums.remove(index)));
+			k -= index * magicNumber;
+		}
+		return result.toString();
+		
+	}
+	
 
+	public static void main(String[] args) {
+		System.out.println(getPermutation1(4, 14));
 	}
 }
---
+++ src/hackerRank/RotateList.java
@@ -0,0 +1,29 @@
+package hackerRank;
+
+/*
+ * Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class RotateList {
+    public static ListNode rotateRight(ListNode head, int k) {
+        
+    }
+}
