6dbdd3820d0aeae3a8a7f1c5f296ec966da6786e
piyush121 <piyushchaudhary07@gmail.com>
2016-06-23 01:06:49 -0400

    Late night code !

---
+++ src/hackerRank/AddBinary.java
@@ -0,0 +1,34 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+/*
+ * Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+ * 
+ */
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class AddBinary {
+
+	 public String addBinary(String a, String b) {
+	        int i = a.length() - 1;
+	        int j = b.length() - 1;
+	        char[] a1 = a.toCharArray();
+	        char[] b1 = b.toCharArray();
+	        while(i <=0 && j >=0) {
+	        	
+	        }
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/hackerRank/PlusOne.java
+++ src/hackerRank/PlusOne.java
@@ -11,28 +11,46 @@ The digits are stored such that the most significant digit is at the head of the
  *
  */
 public class PlusOne {
-	public int[] plusOne(int[] digits) { // easy one. Still took a long time. Sleep makes me more lazy !
-		
-		 int borrow = 1;
-			for (int i = digits.length - 1; i >= 0; i--) {
-				if (borrow == 1 && digits[i]  + 1 <= 9) {
-				    ++digits[i];
-					borrow = 0;
-					break;
-				}
-				else if(borrow == 1 && digits[i] + 1== 10) {
-					digits[i] = 0 ;
-				}
-				
+	public int[] plusOne(int[] digits) { // easy one. Still took a long time.
+											// Sleep makes me more lazy !
+
+		int borrow = 1;
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (borrow == 1 && digits[i] + 1 <= 9) {
+				++digits[i];
+				borrow = 0;
+				break;
+			} else if (borrow == 1 && digits[i] + 1 == 10) {
+				digits[i] = 0;
 			}
-			if(borrow == 1) {
-				int[] res = new int[digits.length + 1];
-				res[0] = 1;
-				return res;
+
+		}
+		if (borrow == 1) {
+			int[] res = new int[digits.length + 1];
+			res[0] = 1;
+			return res;
+		}
+
+		return digits;
+	}
+
+	public int[] plusOneEff(int[] digits) { // More optimized !
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] + 1 <= 9) {
+				++digits[i];
+				break;
+			} else if (digits[i] + 1 == 10) {
+				digits[i] = 0;
 			}
-				
-			
-			return digits;
+
+		}
+		if (digits[0] == 0) {
+			int[] res = new int[digits.length + 1];
+			res[0] = 1;
+			return res;
+		}
+
+		return digits;
 	}
 
 	public static void main(String[] args) {
