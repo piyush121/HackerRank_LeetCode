f5fbe261cbf8d3141075c51052b860298fc608d0
piyush121 <piyushchaudhary07@gmail.com>
2016-06-25 10:51:13 -0400

    Weekend Coding session

---
+++ src/hackerRank/EditDistance.java
@@ -0,0 +1,28 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+/*Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2.
+ *  (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+ */
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+public class EditDistance {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
