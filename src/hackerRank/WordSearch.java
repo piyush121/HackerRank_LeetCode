05b8e330085263647888b9b97de8eecd48a2d415
piyush121 <piyushchaudhary07@gmail.com>
2016-06-28 00:09:26 -0400

    Midnight commit

--- src/hackerRank/WordSearch.java
+++ src/hackerRank/WordSearch.java
@@ -12,18 +12,50 @@ package hackerRank;
  * 
  * For example, Given board =
  * 
- * [ ['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E'] ] word = "ABCCED",
- * -> returns true, word = "SEE", -> returns true, word = "ABCB", -> returns
- * false.
+ * [['A','B','C','E'],
+ *  ['S','F','C','S'], 
+ *  ['A','D','E','E']] 
+ *  word = "ABCCED", -> returns true,
+ *  word = "SEE", -> returns true, 
+ *  word = "ABCB", -> returns false.
  *
  */
 public class WordSearch {
-	public boolean exist(char[][] board, String word) {
-        
+	public static boolean exist(char[][] board, String word) { // Cant do better than this. Most optimal yet !
+		char[] words = word.toCharArray();
+		for(int i = 0 ; i < board.length; i++) {
+			for(int j = 0; j < board[0].length; j++) {
+				if(board[i][j] == words[0])
+					if (DFS(i, j, board, words, 0))
+						return true;
+			}
+		}
+		return false;
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	private static boolean DFS(int row, int col, char[][] board, char[] word, int index) {
+		
+		if(index == word.length)
+			return true;
+		if(row > board.length -1 || row < 0 || col > board[0].length - 1 || col < 0 )
+			return false;
+		if(word[index] != board[row][col])
+			return false;
+		board[row][col] ^= 256;
+		boolean result = DFS(row+1, col, board, word, index + 1)|| DFS(row - 1, col, board, word, index + 1) || DFS(row, col + 1, board, word, index + 1) || DFS(row, col - 1, board, word, index + 1);
 
+		board[row][col] ^= 256;
+
+		
+		return result;
+	}
+	public static void main(String[] args) {
+		char[][] board = {
+				{'A','B','C','E'},
+				{'S','F','C','S'},
+				{'A','D','E','E'}
+						};
+		System.out.println(exist(board,"ABCB"));
 	}
 
 }
