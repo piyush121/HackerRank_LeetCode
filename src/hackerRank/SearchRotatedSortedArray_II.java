3a910666ecbd3e63ceb918c904d36f06e9c0ddba
piyush121 <piyushchaudhary07@gmail.com>
2016-06-29 21:40:49 -0400

    More problems

--- src/hackerRank/SearchRotatedSortedArray_II.java
+++ src/hackerRank/SearchRotatedSortedArray_II.java
@@ -14,15 +14,15 @@ package hackerRank;
  */
 public class SearchRotatedSortedArray_II {
 
-	public static boolean search(int[] nums, int target) {
+	public static boolean search(int[] nums, int target) { //Quiet deep concept !
 		if (nums.length == 0)
 			return false;
 		int low = 0;
 		int high = nums.length - 1;
-		int mid = (low + high) / 2;
 
-		while (low < high) {
-			if (mid == target)
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] == target)
 				return true;
 			if (nums[low] < nums[mid]) { // left half
 											// sorted.
@@ -30,7 +30,7 @@ public class SearchRotatedSortedArray_II {
 					high = mid - 1;
 				else
 					low = mid + 1;
-			} else if (nums[mid] < nums[high]) { // right half sorted.
+			} else if (nums[low] > nums[mid] ) { // right half sorted.
 				if (target > nums[mid] && target <= nums[high])
 					low = mid + 1;
 				else
@@ -44,7 +44,7 @@ public class SearchRotatedSortedArray_II {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(search(new int[] {5,5,1,1,2,3,4,4,4}, 5));
+		System.out.println(search(new int[] {1,3,1,1,1}, 3));
 
 	}
 
