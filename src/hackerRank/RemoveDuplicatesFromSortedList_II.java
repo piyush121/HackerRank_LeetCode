ece5232c8cf0d37dff968e125653d5bb3f12bb3d
piyush121 <piyushchaudhary07@gmail.com>
2016-06-30 00:35:43 -0400

    work

---
+++ src/hackerRank/RemoveDuplicatesFromSortedList_II.java
@@ -0,0 +1,34 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * For example, Given 1->2->3->3->4->4->5, return 1->2->5. Given 1->1->1->2->3,
+ * return 2->3.
+ *
+ */
+/**
+ * Definition for singly-linked list. 
+ * public class ListNode
+ * 	{ 
+ *   int val;
+ *   ListNode * next; 
+ *   ListNode(int x)
+ *   { val = x; }
+ *   }
+ */
+public class RemoveDuplicatesFromSortedList_II {
+
+	public ListNode deleteDuplicates(ListNode head) {
+        
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
