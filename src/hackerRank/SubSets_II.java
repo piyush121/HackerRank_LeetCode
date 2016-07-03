b9c512c7277a7938e7d991553489a810791a3b21
piyush121 <piyushchaudhary07@gmail.com>
2016-07-03 01:05:07 -0400

    Late night efficient coding practice !

--- src/hackerRank/SubSets_II.java
+++ src/hackerRank/SubSets_II.java
@@ -1,4 +1,11 @@
-package hackRank;
+package hackerRank;
+
+import java.util.ArrayList;
+/**
+ * @author Piyush Chaudhary
+ *
+ */
+import java.util.List;
 
 /*Given a collection of integers that might contain duplicates, nums, return all possible subsets.
 
@@ -18,8 +25,36 @@ If nums = [1,2,2], a solution is:
 */
 
 public class SubSets_II {
-     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
-    }
-    
+	public static List<List<Integer>> subsetsWithDup(int[] nums) { //Good one.
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> set = new ArrayList<>();
+		result.add(set);
+		if (nums.length == 0)
+			return result;
+		int dupCount = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			dupCount = 0;
+
+			while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+				dupCount++;
+				i++;
+			}
+			List<List<Integer>> copy = new ArrayList<>();
+			for (int k = 0; k < result.size(); k++) {
+				List<Integer> temp = new ArrayList<>(result.get(k)); // Many things going on here. Read carefully.
+				for (int j = 0; j <= dupCount; j++) {
+					temp.add(nums[i]);
+					copy.add(new ArrayList<>(temp));
+				}
+			}
+			result.addAll(copy);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(subsetsWithDup(new int[] { 1, 2, 2 }));
+	}
+
 }
