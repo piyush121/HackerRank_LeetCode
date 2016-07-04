28974de7f61fce91049fbc3b3df7abdff9a64a34
piyush121 <piyushchaudhary07@gmail.com>
2016-07-04 18:26:53 -0400

    4th of July commit !

--- src/hackerRank/RestoreIPAddress.java
+++ src/hackerRank/RestoreIPAddress.java
@@ -16,19 +16,50 @@ import java.util.List;
  *
  */
 public class RestoreIPAddress { // Wow! that was a great learning curve ! This
-								// solution is not so efficient. but should have
-								// rather go with string instead of string
-								// builder to make solution easier.
+								// solution is not so efficient, but I should have
+								// rather gone with string instead of StringBuilder 
+								//to make solution easier.
 	public static List<String> restoreIpAddresses(String s) {
-		StringBuilder strb = new StringBuilder();
+		String strb = "";
 		List<String> result = new ArrayList<>();
-		helper(s, 1, 0, result, strb);
+		efficientHelper(s, 0, 0, result, strb);
 
 		return result;
 	}
 
+	private static void efficientHelper(String str, int count, int start, List<String> result, String temp) { // This
+																												// one
+																												// is
+																												// efficient
+																												// but
+																												// might
+																												// be
+																												// hard
+																												// to
+																												// code
+																												// up
+																												// in
+																												// an
+																												// interview.
+																												// Time
+		if(count > 4)																							// O(3^4)
+			return;																								
+		if (count == 4 && start == str.length()) {
+			result.add(temp);
+			return;
+		}
+		for (int i = start; i < start + 3 && i < str.length(); i++) {
+			String st = str.substring(start, i + 1);
+			if (st.startsWith("0") && st.length() > 1 || Integer.parseInt(st) > 255)
+				continue;
+			if (count != 3)
+				st = st + ".";
+			efficientHelper(str, count + 1, i + 1, result, temp + st);
+		}
+	}
+
 	private static void helper(String str, int count, int start, List<String> result, StringBuilder temp) {
-		if (count > 4)
+		if(count > 4)
 			return;
 		if (count == 4) {
 			String st = str.substring(start, str.length());
@@ -45,8 +76,7 @@ public class RestoreIPAddress { // Wow! that was a great learning curve ! This
 
 		for (int i = start; i <= start + 3 && i < str.length(); i++) {
 			String st = str.substring(start, i + 1);
-			System.out.println(st + "\t" + temp + "\t" + result);
-			if ((st.startsWith("0") && st.length() > 1) || Integer.parseInt(st) > 255) {
+			if ((st.startsWith("0") && st.length() > 1) || Integer.parseInt(st) > 255) { // Got too complex :P.
 				for (int j = temp.length() - 1; j > 0 && temp.charAt(j) != '.'; j--)
 					temp.deleteCharAt(temp.length() - 1);
 				return;
@@ -59,6 +89,8 @@ public class RestoreIPAddress { // Wow! that was a great learning curve ! This
 			temp.deleteCharAt(temp.length() - 1);
 
 	}
+	
+	
 
 	public static void main(String[] args) {
 		// System.out.println(Integer.parseInt("010"));
