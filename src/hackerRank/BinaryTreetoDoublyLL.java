de1988badd21bf6afed5f038be591b8913582ab8
piyush <piyush@198.105.254.228>
2016-07-11 23:28:28 +0000

    On Arch Linux

--- src/hackerRank/BinaryTreetoDoublyLL.java
+++ src/hackerRank/BinaryTreetoDoublyLL.java
@@ -1,11 +1,11 @@
 package hackerRank;
 
 public class BinaryTreetoDoublyLL {
-	static treeNode last = null; // previous node while traversing tree in an
+	static treeeNode last = null; // previous node while traversing tree in an
 									// in-order fashion.
-	static treeNode head = null; // head pointer in LL.
+	static treeeNode head = null; // head pointer in LL.
 
-	public static void binaryToDLL(treeNode root) { // pretty hard to
+	public static void binaryToDLL(treeeNode root) { // pretty hard to
 													// understand.
 		if (root == null)
 			return;
@@ -21,10 +21,10 @@ public class BinaryTreetoDoublyLL {
 
 	}
 
-	public static treeNode binarytoCLL(treeNode node) { // A different
+	public static treeeNode binarytoCLL(treeeNode node) { // A different
 														// implementation.
-		treeNode leftList = binarytoCLL(node.left);
-		treeNode rightList = binarytoCLL(node.right);
+		treeeNode leftList = binarytoCLL(node.left);
+		treeeNode rightList = binarytoCLL(node.right);
 		node = concat(leftList, node);
 		node = concat(node, rightList);
 
@@ -32,13 +32,13 @@ public class BinaryTreetoDoublyLL {
 
 	}
 
-	public static treeNode concat(treeNode a, treeNode b) {
+	public static treeeNode concat(treeeNode a, treeeNode b) {
 		if (a == null)
 			return b;
 		if (b == null)
 			return a;
-		treeNode aEnd = concat(a, a.left);
-		treeNode bEnd = concat(b, b.left);
+		treeeNode aEnd = concat(a, a.left);
+		treeeNode bEnd = concat(b, b.left);
 		a.left = bEnd;
 		aEnd.right = b;
 		b.left = aEnd;
