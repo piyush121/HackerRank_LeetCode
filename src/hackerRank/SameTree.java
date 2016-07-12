be82043075eb76a9bde93bc1e468134afbb525d2
piyush <piyush@198.105.254.228>
2016-07-12 22:55:50 +0000

    Efficiency

--- src/hackerRank/SameTree.java
+++ src/hackerRank/SameTree.java
@@ -12,8 +12,19 @@ package hackerRank;
  */
 public class SameTree {
 
-	public boolean isSameTree(TreeNode p, TreeNode q) {
+	public boolean isSameTree(TreeNode p, TreeNode q) { // Easy recursive solution.
+		if (p == null && q != null || q == null && p != null)
+			return false;
+		if (p == null)
+			return true;
+		if (p.val != q.val)
+			return false;
+		boolean left = isSameTree(p.left, q.left);
+		if (p.val != q.val)
+			return false;
+		boolean right = isSameTree(p.right, q.right);
 
+		return left && right;
 	}
 
 	public static void main(String[] args) {
