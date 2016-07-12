1be191560e06c95145ec0f21f270b0b53e8cbb78
piyush <piyush@198.105.254.228>
2016-07-12 21:35:58 +0000

    Crack the code.

---
+++ src/hackerRank/SameTree.java
@@ -0,0 +1,24 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given two binary trees, write a function to check if they are equal or not.
+ * 
+ * Two binary trees are considered equal if they are structurally identical and
+ * the nodes have the same value.
+ *
+ */
+public class SameTree {
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
