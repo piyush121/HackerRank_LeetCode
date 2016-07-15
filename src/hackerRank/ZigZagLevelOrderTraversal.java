1405de36f48ccb8cb1c591b3ae593e1a633c3a6f
piyush121 <pc1905@nyu.edu>
2016-07-14 21:53:16 -0400

    Code

---
+++ src/hackerRank/ZigZagLevelOrderTraversal.java
@@ -0,0 +1,33 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * @Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+ *
+ */
+public class ZigZagLevelOrderTraversal {
+	 public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+	        
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
