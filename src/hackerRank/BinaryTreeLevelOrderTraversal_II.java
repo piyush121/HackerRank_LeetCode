0d4db651600639fe40125c757750faf64e9c5b89
piyush121 <pc1905@nyu.edu>
2016-07-16 19:11:27 -0400

    Weekend commit.

---
+++ src/hackerRank/BinaryTreeLevelOrderTraversal_II.java
@@ -0,0 +1,33 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+ *
+ */
+public class BinaryTreeLevelOrderTraversal_II {
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/hackerRank/ConstructBinaryTreeFromPostOrderAndInOrderTraversal.java
+++ src/hackerRank/ConstructBinaryTreeFromPostOrderAndInOrderTraversal.java
@@ -9,7 +9,7 @@ package hackerRank;
  * Note: You may assume that duplicates do not exist in the tree.
  */
 public class ConstructBinaryTreeFromPostOrderAndInOrderTraversal { 
-	public TreeNode buildTree(int[] inorder, int[] postorder) { // Works well enough. But not highly Efficient as its a recursive one.
+	public TreeNode buildTree(int[] inorder, int[] postorder) { // Works well enough. But not super Efficient as its a recursive one.
 		TreeNode root = helper (0, inorder.length - 1, postorder.length - 1, inorder, postorder);
 		return root;
 	}
@@ -21,8 +21,10 @@ public class ConstructBinaryTreeFromPostOrderAndInOrderTraversal {
 		TreeNode node = new TreeNode(postOrder[postEnd]);
 		int index = 0;
 		for(int i = inStart;i <= inEnd; i++) {
-			if(inOrder[i] == postOrder[postEnd]) 
+			if(inOrder[i] == postOrder[postEnd])  {
 				index = i;
+				break; //no need go go forward .
+			}
 		}
 		int rightLen = inEnd - index;
 		node.right = helper (index + 1, inEnd, postEnd - 1, inOrder, postOrder);
@@ -30,6 +32,31 @@ public class ConstructBinaryTreeFromPostOrderAndInOrderTraversal {
 		
 		return node;
 	}
+	
+	 public TreeNode buildTree1(int[] inorder, int[] postorder) { // Works efficiently because of the reverse for loop. :S
+			TreeNode root = helper1(inorder.length - 1,0, postorder.length - 1, inorder, postorder);
+			return root;
+		}
+		
+		private TreeNode helper1(int inStart, int inEnd, int postEnd, int[] inOrder, int[] postOrder){
+			if(inStart < inEnd)
+				return null;
+			
+			TreeNode node = new TreeNode(postOrder[postEnd]);
+			if(inStart == inEnd)
+			    return node;
+			int index = 0;
+			for(int i = inStart;i >= inEnd; i--) {
+				if(inOrder[i] == node.val)  {
+					index = i;
+					break; //no need go forward .
+				}
+			}
+			node.right = helper1 ( inStart, index + 1, postEnd - 1, inOrder, postOrder);
+			node.left = helper1(index - 1, inEnd,  postEnd - (inStart - index) - 1, inOrder, postOrder);
+			
+			return node;
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
