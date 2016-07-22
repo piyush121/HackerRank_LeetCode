3f40db46ff912d5e6dd83f23a39c3b68504cf927
piyush121 <pc1905@nyu.edu>
2016-07-21 20:53:10 -0400

    code

--- src/hackerRank/PascalsTriangle.java
+++ src/hackerRank/PascalsTriangle.java
@@ -3,17 +3,52 @@
  */
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author Piyush Chaudhary
+ * Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
  *
  */
 public class PascalsTriangle {
+	public static List<List<Integer>> generate(int numRows) {
+		
+	}
+	
+	
+	public static List<List<Integer>> generate1(int numRows) { // Efficient but can be made better.
+		List<List<Integer>> res = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
+		if (numRows == 0)
+			return res;
+		list.add(1);
+		res.add(list);
+		for (int i = 1; i < numRows; i++) {
+			list = new ArrayList<>();
+			list.add(1);
+			for (int j = 1; j < i; j++) {
+				list.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+			}
+			list.add(1);
+			res.add(list);
+		}
 
-	/**
-	 * @param args
-	 */
+		return res;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(generate(5));
 
 	}
 
