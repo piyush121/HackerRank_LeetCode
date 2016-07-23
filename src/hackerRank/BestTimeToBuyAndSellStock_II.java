ac22fac1f5a6ac0a9862a066f536e142f8ec6b1c
piyush121 <pc1905@nyu.edu>
2016-07-22 22:06:04 -0400

    code

---
+++ src/hackerRank/BestTimeToBuyAndSellStock_II.java
@@ -0,0 +1,47 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock
+ * on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (ie, buy one and sell one share of the stock
+ * multiple times). However, you may not engage in multiple transactions at the
+ * same time (ie, you must sell the stock before you buy again).
+ *
+ */
+public class BestTimeToBuyAndSellStock_II {
+	public static int maxProfit(int[] prices) {
+			if(prices.length == 0)
+				return 0;
+			int low = prices[0];
+			int minSoFar = prices[0];
+			int profit = 0;
+			
+			for(int i = 1; i < prices.length; i++) {
+				if(prices[i] >= prices[i - 1]) {
+					profit += prices[i - 1] - low;
+					low = prices[i];
+				}
+				else if (prices[i] < prices[i - 1] && i != prices.length - 1) {
+					continue;
+				}
+				else {
+					profit += prices[i] - low
+					}
+				}
+					
+				return profit;
+				
+			}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
