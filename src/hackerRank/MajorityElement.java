4b9fa912c4b23e0c0efbf1b748412fea4a921f7f
piyush121 <pc1905@nyu.edu>
2016-08-01 20:45:19 -0400

    Code.

--- src/hackerRank/MajorityElement.java
+++ src/hackerRank/MajorityElement.java
@@ -12,11 +12,39 @@ package hackerRank;
  *
  */
 public class MajorityElement {
-	public int majorityElement(int[] nums) {
-		int curr = nums[0]; 
+	public int majorityElement(int[] nums) { // Divide and conquer method.
+		return helper(nums, 0, nums.length - 1); //O(nlogn) though.
+	}
+
+	private int helper(int[] nums, int start, int end) {
+		if (start == end)
+			return nums[start];
+		int mid = (start + end) / 2;
+		int left = helper(nums, start, mid);
+		int right = helper(nums, mid + 1, end);
+		if (nums[left] == nums[right])
+			return nums[left];
+		int leftCount = 0;
+		int rightCount = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == left)
+				leftCount++;
+			else if (nums[i] == right)
+				rightCount++;
+		}
+
+		if (leftCount > rightCount)
+			return left;
+		return right;
+	}
+
+	public int majorityElement1(int[] nums) {
+		int curr = nums[0];
 		int count = 1;
 
-		for (int i = 1; i < nums.length; i++) { // Also known as Moore's Voting algorithm.
+		for (int i = 1; i < nums.length; i++) { // Also known as Moore's Voting
+												// algorithm.
 			if (nums[i] == curr) {
 				count++;
 				continue;
