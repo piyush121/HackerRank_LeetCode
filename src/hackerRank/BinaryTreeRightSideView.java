f32b67c4716d921d31db5a5b7dcdfe4ba4b7433f
piyush121 <pc1905@nyu.edu>
2016-08-03 23:20:16 -0400

    Code.

---
+++ src/hackerRank/BinaryTreeRightSideView.java
@@ -0,0 +1,28 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+ *
+ */
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        	
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
