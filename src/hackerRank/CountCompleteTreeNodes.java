ffc60ed69b896a3dbe75eea8f66e35d44e18cd11
piyush121 <pc1905@nyu.edu>
2016-08-10 21:49:26 -0400

    Ant

---
+++ src/hackerRank/CountCompleteTreeNodes.java
@@ -0,0 +1,25 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a complete binary tree, count the number of nodes.
+ * 
+ * Definition of a complete binary tree from Wikipedia: In a complete binary
+ * tree every level, except possibly the last, is completely filled, and all
+ * nodes in the last level are as far left as possible. It can have between 1
+ * and 2h nodes inclusive at the last level h.
+ *
+ */
+public class CountCompleteTreeNodes {
+	public int countNodes(TreeNode root) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
