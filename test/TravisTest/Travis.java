f403ae2a3a7d3237b935a2c6de7502a88a441dcb
piyush121 <pc1905@nyu.edu>
2016-08-10 21:12:52 -0400

    Ant fixes

--- test/TravisTest/Travis.java
+++ test/TravisTest/Travis.java
@@ -2,10 +2,9 @@ package TravisTest;
 
 import static org.junit.Assert.*;
 import hackerRank.CompareVersionNumbers;
-import org.junit.Test;
 
 public class Travis {
-		@Test
+		@org.junit.Test
 		public void testHellowWorld(){
 			assertEquals(1, CompareVersionNumbers.compareVersion("1.1", "01"));
 		}
