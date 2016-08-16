aee1511029cd54e20b27b027c98fecd11d3be9a7
piyush121 <pc1905@nyu.edu>
2016-08-15 21:21:48 -0400

    stackkkk

--- src/hackerRank/BasicCalculator_II.java
+++ src/hackerRank/BasicCalculator_II.java
@@ -20,29 +20,40 @@ Note: Do not use the eval built-in library function.
  *
  */
 public class BasicCalculator_II {
-	 public int calculate(String s) {
-	        Stack<Integer> stack = new Stack<>();
-	        int num = 0;
-	        int sign = '+';	        
-	        for(int i = 0; i < s.length() - 1; i++) {
-	        	char ch = s.charAt(i);
-	        	if(ch == ' ')
-	        		continue;
-	        	if(Character.isDigit(ch))
-	        		num = 10 * num + ch - '0';
-	        	else if ( sign == '+') 
-	        		stack.push(num);
-	        	else if ( sign == '-') 
-	        		stack.push(-num);
-	        	else if ( sign == '/') { 
-	        		stack.push(num);
-	        	}
-	        	sign = s.charAt(i + 1);
-	        }
-	    }
+	public static int calculate(String s) {
+		Stack<Integer> stack = new Stack<>();
+		int num = 0;
+		char sign = '+';
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == ' ')
+				continue;
+			if (Character.isDigit(ch)) {
+				num = 10 * num + (ch - '0');
+				if (i == s.length() - 1 || !Character.isDigit(s.charAt(i + 1))) {
+					if (sign == '+')
+						stack.push(num);
+					else if (sign == '-')
+						stack.push(-num);
+					else if (sign == '/') {
+						stack.push(stack.pop() / num);
+					} else if (sign == '*') {
+						stack.push(stack.pop() * num);
+					}
+				}
+			} else {
+				sign = ch;
+				num = 0;
+			}
+		}
+		int res = 0;
+		while (!stack.isEmpty()) {
+			res += stack.pop();
+		}
+		return res;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(calculate("42"));
 	}
 
 }
