3501fdaad416daa0bec20fc57fddc793a055faf2
piyush121 <pc1905@nyu.edu>
2016-08-17 20:02:22 -0400

    Just do it !

--- src/hackerRank/MajorityElement_II.java
+++ src/hackerRank/MajorityElement_II.java
@@ -17,33 +17,42 @@ import java.util.List;
  */
 public class MajorityElement_II {
 	public List<Integer> majorityElement(int[] nums) {
-		List<Integer> res = new ArrayList<Integer>();
+		List<Integer> res = new ArrayList<>();
 		if (nums.length == 0)
 			return res;
-		Integer num1 = null, num2 = null;
-		int count1 = 0, count2 = 0;
+
+		int num1 = nums[0];
+		int num2 = nums[0];
+		int count1 = 1;
+		int count2 = 0;
 
 		for (int val : nums) {
 			if (val == num1)
 				count1++;
-			else if (val == count2)
+			else if (val == num2)
 				count2++;
-			else {
+			else if (count1 == 0) {
+				num1 = val;
+				count1++;
+			} else if (count2 == 0) {
+				num2 = val;
+				count2++;
+			} else {
 				count1--;
 				count2--;
 			}
-
-			if (count1 <= 0)
-				num1 = val;
-			else if (count2 <= 0 && val != num1)
-				num2 = val;
-
 		}
-		int count = nums.length / 3;
-
-		if (count1 >= count)
+		count1 = 0;
+		count2 = 0;
+		for (int val : nums) {
+			if (val == num1)
+				count1++;
+			else if (val == num2)
+				count2++;
+		}
+		if (count1 > nums.length / 3)
 			res.add(num1);
-		if (count2 >= count)
+		if (count2 > nums.length / 3)
 			res.add(num2);
 		return res;
 	}
