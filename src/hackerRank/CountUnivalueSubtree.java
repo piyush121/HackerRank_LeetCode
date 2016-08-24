045541434c5b294b953b6f91e8bcffa30f44a6ad
piyush121 <pc1905@nyu.edu>
2016-08-23 20:51:04 -0400

    work!

--- src/hackerRank/CountUnivalueSubtree.java
+++ src/hackerRank/CountUnivalueSubtree.java
@@ -19,7 +19,35 @@ return 4.
  *
  */
 public class CountUnivalueSubtree {
-	public int countUnivalSubtrees(TreeNode root) { // Less efficient but works !
+	
+	public int countUnivalSubtrees(TreeNode root) { // Efficient.
+		if (root == null)
+			return 0;
+		int[] count = new int[1]; // to pass reference object.
+		helper(root, count);
+		return count[0];
+	}
+
+	private boolean helper(TreeNode node, int[] count) {
+		if (node == null)
+			return true;
+		boolean left = helper(node.left, count);
+		boolean right = helper(node.right, count);
+
+		if (left && right) {
+			if (node.left != null && node.val != node.left.val)
+				return false;
+			if (node.right != null && node.val != node.right.val)
+				return false;
+			count[0]++;
+			return true;
+		}
+
+		return false;
+	}
+
+	public int countUnivalSubtrees1(TreeNode root) { // Less efficient but works
+														// !
 		if (root == null)
 			return 0;
 		boolean left = helper(root.left, root.val);
@@ -27,11 +55,11 @@ public class CountUnivalueSubtree {
 		if (left && right)
 			return count(root);
 		else if (left)
-			return count(root.left) + countUnivalSubtrees(root.right);
+			return count(root.left) + countUnivalSubtrees1(root.right);
 		else if (right)
-			return count(root.right) + countUnivalSubtrees(root.left);
+			return count(root.right) + countUnivalSubtrees1(root.left);
 		else
-			return countUnivalSubtrees(root.left) + countUnivalSubtrees(root.right);
+			return countUnivalSubtrees1(root.left) + countUnivalSubtrees1(root.right);
 	}
 
 	private boolean helper(TreeNode node, int val) {
