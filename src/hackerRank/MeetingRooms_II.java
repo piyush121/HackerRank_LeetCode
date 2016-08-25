63d6822765a2d50dc72c17f1a7ee1f32e3d8a935
piyush121 <pc1905@nyu.edu>
2016-08-24 22:29:53 -0400

    More work.

--- src/hackerRank/MeetingRooms_II.java
+++ src/hackerRank/MeetingRooms_II.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Given an array of meeting time intervals consisting of start and end times
@@ -17,7 +18,36 @@ import java.util.List;
  *
  */
 public class MeetingRooms_II {
-	public int minMeetingRooms(Interval[] intervals) { // Not highly efficient but works !
+	public int minMeetingRooms(Interval[] intervals) {
+		if (intervals.length == 0)
+			return 0;
+		Arrays.sort(intervals, new Comparator<Interval>() {
+			public int compare(Interval o1, Interval o2) {
+				return o1.start - o2.start;
+			}
+		});
+		PriorityQueue<Interval> que = new PriorityQueue<>(new Comparator<Interval>() {
+			public int compare(Interval o1, Interval o2) {
+				return o1.end - o2.end;
+			}
+		});
+		que.offer(intervals[0]);
+		int count = 1;
+		for (int i = 1; i < intervals.length; i++) {
+			Interval temp = que.poll();
+			if (intervals[i].start >= temp.end) { // the next lowest start interval is supposed to be added to earliest
+				temp.end = intervals[i].end; // finishing interval. If it has a conflict with that interval that means
+			} else { 						// it will also have conflict with other seen intervals. Deep greedy thinking requied.
+				que.offer(intervals[i]);
+				count++;
+			}
+			que.offer(temp);
+		}
+		return count;
+	}
+
+	public int minMeetingRooms1(Interval[] intervals) { // Not highly efficient
+														// but works !
 		if (intervals.length == 0)
 			return 0;
 		Arrays.sort(intervals, new Comparator<Interval>() {
@@ -32,7 +62,11 @@ public class MeetingRooms_II {
 			int size = list.size();
 			for (int j = 0; j <= size; j++) {
 				if (j != size && list.get(j).end < intervals[i].start) {
-					list.get(j).end = intervals[i].end; // Check if this interval can fit into one of the meeting rooms already acquired.
+					list.get(j).end = intervals[i].end; // Check if this
+														// interval can fit into
+														// one of the meeting
+														// rooms already
+														// acquired.
 					break;
 				}
 				if (j == size) { // It needs new meeting room.
