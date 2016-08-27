83d43ef79d203d432f0459a0bb0afe386eb4f2b9
piyush121 <pc1905@nyu.edu>
2016-08-27 18:04:36 -0400

    work!

---
+++ src/hackerRank/ThreeSumSmaller.java
@@ -0,0 +1,45 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of n integers nums and a target, find the number of index
+ * triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] +
+ * nums[j] + nums[k] < target.
+ * 
+ * For example, given nums = [-2, 0, 1, 3], and target = 2.
+ * 
+ * Return 2. Because there are two triplets which sums are less than 2:
+ * 
+ * [-2, 0, 1] [-2, 0, 3]
+ *
+ */
+public class ThreeSumSmaller {
+	public int threeSumSmaller(int[] nums, int target) {
+		if (nums.length == 0)
+			return 0;
+		int res = 0;
+		Arrays.sort(nums);
+		int start = 0;
+		while (start < nums.length - 2) {
+			int curr = start + 1;
+			int end = nums.length - 1;
+			while (curr < end) {
+				if (nums[curr] + nums[start] + nums[end] < target) {
+					res += end - curr;
+					curr++;
+				} else
+					end--;
+			}
+			start++;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new ThreeSumSmaller().threeSumSmaller(new int[] { 0, -2, 1, 1, 2 }, 1));
+	}
+}
