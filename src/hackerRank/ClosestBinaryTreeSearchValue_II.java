af949a7a21937056fc98aaf4b8707b78f2c04897
piyush121 <pc1905@nyu.edu>
2016-09-01 17:57:15 -0400

    Hard one !

--- src/hackerRank/ClosestBinaryTreeSearchValue_II.java
+++ src/hackerRank/ClosestBinaryTreeSearchValue_II.java
@@ -3,7 +3,9 @@
  */
 package hackerRank;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Given a non-empty binary search tree and a target value, find k values in the
@@ -27,7 +29,65 @@ import java.util.List;
  *
  */
 public class ClosestBinaryTreeSearchValue_II {
-	public List<Integer> closestKValues(TreeNode root, double target, int k) {
+	public List<Integer> closestKValues(TreeNode root, double target, int k) { // Most
+																				// elegant
+																				// and
+																				// efficient
+																				// solution.
+		List<Integer> res = new ArrayList<Integer>();
+		helper(root, target, k, res);
+		return res;
+	}
 
+	private void helper(TreeNode root, double target, int k, List<Integer> res) {
+		if (root == null)
+			return;
+		helper(root.left, target, k, res);
+
+		if (res.size() == k) { // keep the window of size `k`.
+			if (Math.abs(target - res.get(0)) > Math.abs(target - root.val)) {
+				res.remove(0);
+				res.add(root.val);
+			} else
+				return; // No need to go forward as target - higher number will
+						// always be more.
+
+		}
+		helper(root.right, target, k, res);
+
+	}
+
+	public List<Integer> closestKValues1(TreeNode root, double target, int k) {
+		Stack<Integer> smaller = new Stack<>();
+		Stack<Integer> bigger = new Stack<>();
+		inorder(root, false, target, smaller); // Predecessors.
+		inorder(root, true, target, bigger); // Sucecessors.
+		List<Integer> res = new ArrayList<>();
+
+		while (res.size() < k) {
+			if (bigger.isEmpty())
+				res.add(smaller.pop());
+			else if (smaller.isEmpty())
+				res.add(bigger.pop());
+
+			else if (target - smaller.peek() <= bigger.peek() - target)
+				res.add(smaller.pop());
+			else if (target - smaller.peek() > bigger.peek() - target)
+				res.add(bigger.pop());
+
+		}
+
+		return res;
+	}
+	// We are essential trying to build two stack one with all smaller nodes than target and one with bigger ones.
+	private void inorder(TreeNode node, boolean reverse, double target, Stack<Integer> stk) {
+		if (node == null)
+			return;
+		inorder(reverse ? node.right : node.left, reverse, target, stk);
+		// No need to go forward.
+		if (!reverse && node.val > target || reverse && node.val <= target)
+			return;
+		stk.push(node.val);
+		inorder(reverse ? node.left : node.right, reverse, target, stk);
 	}
 }
