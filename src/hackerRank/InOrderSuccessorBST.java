45ded71497b23ea86f3b994409dcbb74da5cdd53
piyush121 <pc1905@nyu.edu>
2016-09-11 14:12:55 -0400

    In progress.

---
+++ src/hackerRank/InOrderSuccessorBST.java
@@ -0,0 +1,18 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a binary search tree and a node in it, find the in-order successor of
+ * that node in the BST.
+ * 
+ * Note: If the given node has no in-order successor in the tree, return null.
+ *
+ */
+public class InOrderSuccessorBST {
+	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		if(root == null)
+			return null;
+	}
+}
