996c985a06b10660a816dfa6aa347e1768294ea1
piyush121 <pc1905@nyu.edu>
2016-09-12 16:44:54 -0400

    Stuck!

--- src/hackerRank/InOrderSuccessorBST.java
+++ src/hackerRank/InOrderSuccessorBST.java
@@ -12,7 +12,34 @@ package hackerRank;
  */
 public class InOrderSuccessorBST {
 	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-		if(root == null)
-			return null;
+		TreeNode succ = null;
+		while (root != null) {
+			if (root.val > p.val) {
+				succ = root;
+				root = root.left;
+			}
+
+			else
+				root = root.right;
+		}
+
+		return succ;
 	}
+	/*
+	 * The idea is to compare root's value with p's value if root is not null,
+	 * and consider the following two cases:
+	 * 
+	 * 1) root.val > p.val. In this case, root can be a possible answer, so we
+	 * store the root node first and call it res. However, we don't know if
+	 * there is anymore node on root's left that is larger than p.val. So we
+	 * move root to its left and check again.
+	 * 
+	 * 2) root.val <= p.val. In this case, root cannot be p's inorder successor,
+	 * neither can root's left child. So we only need to consider root's right
+	 * child, thus we move root to its right and check again.
+	 * 
+	 * We continuously move root until exhausted. To this point, we only need to
+	 * return the res in case 1.
+	 * 
+	 */
 }
