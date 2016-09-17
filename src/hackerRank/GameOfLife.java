7e4373699138d5fd1b4ce4542233fb99e258eac2
piyush121 <pc1905@nyu.edu>
2016-09-17 00:01:40 -0400

    Toughie!

--- src/hackerRank/GameOfLife.java
+++ src/hackerRank/GameOfLife.java
@@ -24,25 +24,50 @@ In this question, we represent the board using a 2D array. In principle, the boa
 would cause problems when the active area encroaches the border of the array. How would you address 
 these problems?
  *
+ *
+ *Solution : 
+ *To solve it in place, we use 2 bits to store 2 states:
+
+[2nd bit, 1st bit] = [next state, current state]
+
+- 00  dead (next) <- dead (current)
+- 01  dead (next) <- live (current)  
+- 10  live (next) <- dead (current)  
+- 11  live (next) <- live (current) 
  */
 public class GameOfLife {
-	 public void gameOfLife(int[][] board) {
-		 int rows = board.length;
-		 int cols = board[0].length;
-	        for(int i = 0; i < rows; i++)
-	        	for(int j = 0 ; j < cols; j++) {
-	        		
-	        	}
-		 
-	    }
-	 
-	 private int getLiveNeighbors(int[][] board, int x , int y) {
-		 int lives = 0;
-		 for(int i = x - 1; i <= x + 1; i++)
-			 for(int j = y - 1; j <= y + 1; j++) {
-				 if((board[i][j] & 1) == 1)
-					 lives++;
-			 }
-		 return lives;
-	 }
+	public void gameOfLife(int[][] board) {
+		int rows = board.length;
+		int cols = board[0].length;
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++) {
+				int lives = getLiveNeighbors(board, i, j);
+				if ((board[i][j] & 1) == 1) {
+					if (lives < 2)
+						board[i][j] = board[i][j] & 1;
+					else if (lives == 2 || lives == 3)
+						board[i][j] = board[i][j] | 2;
+					else if (lives > 3)
+						board[i][j] = board[i][j] & 1;
+				} else if (lives == 3)
+					board[i][j] = board[i][j] | 2;
+
+			}
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				board[i][j] >>= 1;
+
+	}
+
+	private int getLiveNeighbors(int[][] board, int x, int y) {
+		int lives = 0;
+		for (int i = Math.max(0, x - 1); i <= Math.min(board.length - 1, x + 1); i++)
+			for (int j = Math.max(0, y - 1); j <= Math.min(board[0].length - 1, y + 1); j++) {
+				if (i == x && j == y)
+					continue;
+				if ((board[i][j] & 1) == 1)
+					lives++;
+			}
+		return lives;
+	}
 }
