6499ce1b3e5f8fe8ad847ec4cdf92d761ada6d37
piyush121 <pc1905@nyu.edu>
2016-09-17 21:50:12 -0400

    working...

---
+++ src/hackerRank/N_Queens.java
@@ -0,0 +1,37 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+import java.util.List;
+
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n Chess board such that no two queens 
+ * attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both 
+indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+ *
+ */
+public class N_Queens {
+	public List<List<String>> solveNQueens(int n) {
+        
+    }
+}
