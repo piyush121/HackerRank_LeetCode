d742c3988159113ca2722ae41932fb1ddc77989a
piyush121 <pc1905@nyu.edu>
2016-09-23 00:15:24 -0400

    D.P. can make your life hard sometimes!

--- src/hackerRank/DecodeWays.java
+++ src/hackerRank/DecodeWays.java
@@ -22,7 +22,30 @@ The number of ways decoding "12" is 2.
  *
  */
 public class DecodeWays {
-	public int numDecodings(String s) { // Inefficient backtracking solution.
+
+	public int numDecodings(String s) {// Dynamic programming way! Tedious one.
+		if (s.length() < 1)
+			return 0;
+		int DP[] = new int[s.length() + 1];
+		DP[0] = 1; // Initialization
+		DP[1] = s.charAt(0) == '0' ? 0 : 1;
+		for (int i = 2; i <= s.length(); i++) {
+			String str = s.substring(i - 2, i); // last two digit number
+			int val2 = Integer.valueOf(str);
+			int val1 = s.charAt(i - 1) - '0'; // last 1 digit number
+
+			if (val1 > 0)
+				DP[i] += DP[i - 1];
+			if (val2 >= 10 && val2 <= 26) // tricky part.
+				DP[i] += DP[i - 2];
+		}
+
+		return DP[s.length()];
+
+	}
+	
+	
+	public int numDecodings1(String s) { // Inefficient backtracking solution.
 		if (s.length() == 0)
 			return 0;
 		Map<String, Integer> map = new HashMap<>();
