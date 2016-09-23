5e4bbcdb3b50c0ca038ecc7654b218c41aa62891
piyush121 <pc1905@nyu.edu>
2016-09-23 19:38:19 -0400

    Just do it.

---
+++ src/hackerRank/Test.java
@@ -0,0 +1,18 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+import java.util.*;
+/**
+ * Testing Java features.
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+		String str1 = "hello";
+		String str2 = "hello1";
+		System.out.println(str1.compareTo(str2));
+	}
+
+}
