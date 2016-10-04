b6e7c78754aa13e573319d3d4f318061e27680e8
piyush121 <pc1905@nyu.edu>
2016-10-04 12:38:32 -0400

    Missing ranges.

--- src/hackerRank/MissingRanges.java
+++ src/hackerRank/MissingRanges.java
@@ -3,6 +3,9 @@
  */
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given a sorted integer array where the range of elements are [lower, upper] inclusive, return its 
  * missing ranges.
@@ -12,6 +15,25 @@ For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4-
  */
 public class MissingRanges {
 	public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+
+        List<String> res = new ArrayList<>();
         
+        for(int i = 0 ; i < nums.length; i++) {
+        	if(nums[i] > lower) {
+        		if(lower == nums[i] - 1)
+        			res.add(""+lower);
+        		else
+        			res.add(lower + "->" + (nums[i] - 1));
+        	}
+        	lower = nums[i] + 1; // key part here. Keep increasing the lower until you hit the spot.
+        }
+        
+        if(upper >= lower) {
+    		if(lower == upper)
+    			res.add(""+lower);
+    		else
+    			res.add(lower + "->" + upper);
+    	}
+        return res;
     }
 }
