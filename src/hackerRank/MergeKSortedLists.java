3644957233964eadbec598b3af040647d09ab23c
piyush121 <pc1905@nyu.edu>
2016-10-05 12:41:40 -0400

    Heapsss.

--- src/hackerRank/MergeKSortedLists.java
+++ src/hackerRank/MergeKSortedLists.java
@@ -3,13 +3,17 @@
  */
 package hackerRank;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and
  * describe its complexity.
  * 
  */
 public class MergeKSortedLists {
-	public ListNode mergeKLists(ListNode[] lists) { // Brutefirce way. but works well !
+	public ListNode mergeKLists1(ListNode[] lists) { // Brutefirce way. but
+														// works well !
 		ListNode dummy = new ListNode(0);
 		ListNode ptr = dummy;
 		int nullCounter = 0;
@@ -37,6 +41,32 @@ public class MergeKSortedLists {
 			ptr = ptr.next;
 		}
 
+		return dummy.next;
+	}
+
+	public ListNode mergeKLists(ListNode[] lists) { // Priority Queue based
+													// efficient solution. O(nklogk)
+
+		PriorityQueue<ListNode> que = new PriorityQueue<>(new Comparator<ListNode>() {
+			public int compare(ListNode obj1, ListNode obj2) {
+				return obj1.val - obj2.val;
+			}
+		});
+
+		for (ListNode node : lists) {
+			if (node != null)
+				que.add(node);
+		}
+		ListNode dummy = new ListNode(0);
+		ListNode ptr = dummy;
+		while (!que.isEmpty()) {
+			ListNode node = que.poll();
+			ptr.next = node;
+			ptr = ptr.next;
+			if (node.next != null) // Don't add null into PQ.
+				que.add(node.next);
+
+		}
 		return dummy.next;
 	}
 }
