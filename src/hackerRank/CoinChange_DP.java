d4bca2af3d9e04c23841135b189ee2a776d2e810
piyush121 <pc1905@nyu.edu>
2016-10-10 18:17:13 -0400

    More...

--- src/hackerRank/CoinChange_DP.java
+++ src/hackerRank/CoinChange_DP.java
@@ -3,6 +3,7 @@
  */
 package hackerRank;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -21,6 +22,21 @@ public class CoinChange_DP {
             }
         }       
         return DP[money];
+    }
+	
+	public static long makeChange1(int[] coins, int money) { // O(m * n ) space.
+        long[][] DP = new long[coins.length + 1][money + 1];
+        Arrays.fill(DP[0], 0);
+        for(int i = 0 ; i < DP.length; i++) 
+            DP[i][0] = 1;
+        for(int i = 1 ; i < DP.length; i++) {
+            int val = coins[i - 1];
+            for(int j = 1; j < DP[0].length; j++) {
+                    DP[i][j] = (j - val >= 0 ? DP[i][j - val] : 0) + DP[i - 1][j];
+            }
+        }
+        
+        return DP[coins.length][money];
     }
     
     public static void main(String[] args) {
