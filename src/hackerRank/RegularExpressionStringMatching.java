b999d3846dced44001c41372030501eece8440b3
piyush121 <pc1905@nyu.edu>
2016-10-16 20:58:12 -0400

    In progress...

--- src/hackerRank/RegularExpressionStringMatching.java
+++ src/hackerRank/RegularExpressionStringMatching.java
@@ -27,27 +27,10 @@ isMatch("aab", "c*a*b") → true
 public class RegularExpressionStringMatching {
 	public boolean isMatch(String s, String p) {
 
-		return (isMatch(s, p, '#', 0));
-
 	}
 
 	public boolean isMatch(String s, String p, char prev, int start) {
-		if (s.length() == 0 && start == p.length())
-			return true;
-		if (s.length() == 0 && start < p.length())
-			return false;
-		char chs = s.charAt(0);
 
-		for (int i = start; i < p.length(); i++) {
-			char chp = p.charAt(i);
-			if (chp >= 'a' && chp <= 'z' && chs != chp && p.charAt(i + 1) == '*')
-				return isMatch(s.substring(1), p, '#', i + 2);
-
-			if (chs == chp || chp == '.' || (chp == '*' && (prev == chs || prev == '.'))) {
-				boolean res = isMatch(s.substring(1), p, chp, i + 1);
-				if (res)
-					return true;
-			}
 		}
 		return false;
 	}
