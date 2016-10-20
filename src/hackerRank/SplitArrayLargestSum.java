0cc517d78ec53e4322edf67650e85528c6157ad6
piyush121 <pc1905@nyu.edu>
2016-10-20 00:44:25 -0400

    Hard ques. makes you think deep!

--- src/hackerRank/SplitArrayLargestSum.java
+++ src/hackerRank/SplitArrayLargestSum.java
@@ -23,7 +23,56 @@ package hackerRank;
  *
  */
 public class SplitArrayLargestSum {
-	public int splitArray(int[] nums, int m) { // Inefficient Solution.
+
+	public int splitArray(int[] nums, int m) { // Highly efficient and tricky
+												// solution! O(nlogn) time.
+		int max = -1;
+		long sum = 0L;
+		for (int val : nums) {
+			if (val > max)
+				max = val;
+			sum += val;
+		}
+		if (m == 1)
+			return (int) sum;
+		return binarySearch(nums, max, sum, m);
+	}
+
+	private int binarySearch(int[] nums, long start, long end, int m) {
+		long mid = 0L;
+
+		while (start < end) {
+			mid = (start + end) / 2;
+			if (isValid(nums, mid, m)) { // check this val.
+				end = mid; // This val worked so we might found smaller minimum
+							// sum which is also the max.
+			} else
+				start = mid + 1;
+		}
+
+		return (int) end;
+	}
+
+	private boolean isValid(int[] nums, long max, int m) { // Check if you can
+															// use this value to
+															// make m subarrays
+															// each having sum
+															// <= this val.
+		int curr = 0;
+		int count = 1;
+		for (int val : nums) {
+			curr += val;
+			if (curr > max) {
+				curr = val;
+				count++;
+				if (count > m)
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public int splitArray1(int[] nums, int m) { // Inefficient Solution.
 		int[] DP = new int[nums.length + 1];
 		DP[0] = 0;
 		for (int i = 0; i < nums.length; i++)
