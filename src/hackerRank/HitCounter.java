54c406a3bde1ecd82693a29a9062e435b1729db3
piyush121 <pc1905@nyu.edu>
2016-10-26 18:10:19 -0400

    Minor optimizations.

--- src/hackerRank/HitCounter.java
+++ src/hackerRank/HitCounter.java
@@ -43,41 +43,49 @@ What if the number of hits per second could be very large? Does your design scal
  */
 public class HitCounter {
 	int[] hits;
-    int[] times;
-    int prevStamp; // placeholder :P
-    /** Initialize your data structure here. */
-    public HitCounter() {
-        hits = new int[300];
-        times = new int[300]; // timestamps of all hits.
-        prevStamp = 0;
-        
-    }
-    
-    /** Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    public void hit(int timestamp) {
-        
-        if( times[timestamp % 300] == timestamp) { // check if this timestamp is same as previous one.
-            hits[timestamp % 300]++;
-        }
-        else {
-            hits[timestamp % 300] = 1;
-            times[timestamp % 300] = timestamp;
-        }
-        prevStamp = timestamp;
-        
-    }
-    
-    /** Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    public int getHits(int timestamp) {
-        
-        int res = 0;
-        for(int i = 0 ; i < 300; i++) { //O(1).
-            if(times[i] > timestamp - 300)
-            res += hits[i];
-        }
-        return res;
-    }
+	int[] times;
+	int prevStamp; // placeholder :P
+
+	/** Initialize your data structure here. */
+	public HitCounter() {
+		hits = new int[300];
+		times = new int[300]; // timestamps of all hits.
+		prevStamp = 0;
+
+	}
+
+	/**
+	 * Record a hit.
+	 * 
+	 * @param timestamp
+	 *            - The current timestamp (in seconds granularity).
+	 */
+	public void hit(int timestamp) {
+		int idx = timestamp % 300;
+		if (times[idx] == timestamp) {
+			hits[idx]++;
+		} else {
+			hits[idx] = 1;
+			times[idx] = timestamp;
+		}
+		prevStamp = timestamp;
+
+	}
+
+	/**
+	 * Return the number of hits in the past 5 minutes.
+	 * 
+	 * @param timestamp
+	 *            - The current timestamp (in seconds granularity).
+	 */
+	public int getHits(int timestamp) {
+
+		int res = 0;
+		for (int i = 0; i < 300; i++) { // O(1).
+			if (times[i] > timestamp - 300)
+				res += hits[i];
+		}
+		return res;
+	}
 
 }
