f2d16a815bd2c54eb58d125d348f24e9c97a9f00
piyush121 <pc1905@nyu.edu>
2016-11-01 12:27:38 -0400

    still fighting !

--- src/hackerRank/BurstBalloons.java
+++ src/hackerRank/BurstBalloons.java
@@ -3,6 +3,9 @@
  */
 package hackerRank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by 
  * array nums. You are asked to burst all the balloons. If the you burst balloon i you will get 
@@ -27,8 +30,44 @@ Return 167
  */
 public class BurstBalloons {
 	public int maxCoins(int[] nums) {
-        
-    }
+		if (nums.length == 0)
+			return 0;
+		if (nums.length == 1)
+			return nums[0];
+		Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0)
+				map.put(nums[i], new int[] { 1, nums[i + 1] });
+			else if (i == nums.length - 1)
+				map.put(nums[i], new int[] { nums[i - 1], 1 });
+			else
+				map.put(nums[i], new int[] { nums[i - 1], nums[i + 1] });
+		}
+		return maxCoin(map, nums, 0, nums.length - 1);
+
+	}
+
+	public int maxCoin(Map<Integer, int[]> map, int[] nums, int start, int end) {
+		if (start > end)
+			return 0;
+		int max = 0;
+		for (int i = start; i <= end; i++) {
+			int res = 0;
+			int left = map.get(nums[i])[0];
+			int right = map.get(nums[i])[1];
+			res += left * nums[i] * right;
+			map.get(left)[1] = right;
+			map.get(right)[0] = left;
+			res += maxCoin(map, nums, start + 1, end);
+			res += maxCoin(map, nums, start, i - 1);
+			map.get(left)[1] = nums[i];
+			map.get(right)[0] = nums[i];
+			if (res > max)
+				max = res;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
