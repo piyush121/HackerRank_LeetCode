97a7d5208e72452e230a7deaf15340ddc3a41d1d
piyush121 <pc1905@nyu.edu>
2016-11-04 18:00:30 -0400

    parabola...

--- src/hackerRank/SortTransformedArray.java
+++ src/hackerRank/SortTransformedArray.java
@@ -22,7 +22,33 @@ Result: [-23, -5, 1, 7]
  *
  */
 public class SortTransformedArray {
-	//public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-        
-    //}
+	public int[] sortTransformedArray(int[] nums, int a, int b, int c) { // It's a parabola equation.
+		if (nums.length == 0)
+			return new int[] {};
+		int[] res = new int[nums.length];
+		int i = 0, j = nums.length - 1;
+		int ptr = a >= 0 ? nums.length - 1 : 0;
+		while (i <= j) {
+			if (a >= 0) {
+				res[ptr--] = getVal(nums[i], a, b, c) > getVal(nums[j], a, b, c) ? getVal(nums[i++], a, b, c)
+						: getVal(nums[j--], a, b, c);
+			} else {
+				res[ptr++] = getVal(nums[i], a, b, c) < getVal(nums[j], a, b, c) ? getVal(nums[i++], a, b, c)
+						: getVal(nums[j--], a, b, c);
+			}
+
+		}
+		return res;
+	}
+
+	public int getVal(int x, int a, int b, int c) {
+		return a * x * x + b * x + c;
+	}
+	/*
+	 * The idea is simple:
+For a parabola,
+if a >= 0, the min value is at its vertex. So our our sorting should goes from two end 
+points towards the vertex, HIGH to LOW.
+if a < 0, the max value is at its vertex. So our sort goes LOW to HIGH.
+	 */
 }
