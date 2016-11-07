c3b921bc88db7704000c9a179724c7cb4b9e7270
piyush121 <pc1905@nyu.edu>
2016-11-06 19:50:12 -0500

    Mid Term commit.

---
+++ src/hackerRank/TrappingRainWater.java
@@ -0,0 +1,18 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+Image reference :
+http://www.leetcode.com/wp-content/uploads/2012/08/rainwatertrap.png
+For example, 
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ *
+ */
+public class TrappingRainWater {
+	 public int trap(int[] height) {
+	        
+	    }
+}
