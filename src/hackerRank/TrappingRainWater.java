ed6ea5856ecaa1b2841708c9964d18abc395e928
piyush121 <pc1905@nyu.edu>
2016-11-08 13:42:48 -0500

    US Election commit!

--- src/hackerRank/TrappingRainWater.java
+++ src/hackerRank/TrappingRainWater.java
@@ -3,6 +3,8 @@
  */
 package hackerRank;
 
+import java.util.Stack;
+
 /**
  * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
 Image reference :
@@ -12,7 +14,22 @@ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
  *
  */
 public class TrappingRainWater {
-	 public int trap(int[] height) {
-	        
-	    }
+	public int trap(int[] height) {
+		Stack<Integer> bars = new Stack<>();
+		int i = 0;
+		int ans = 0;
+		while (i < height.length) {
+			if (bars.isEmpty() || height[bars.peek()] >= height[i])
+				bars.push(i++);
+			else {
+				while (!bars.isEmpty() && height[bars.peek()] < height[i]) {
+					int bar = height[bars.pop()];
+					ans += bars.isEmpty() ? 0
+							: (Math.min(height[bars.peek()], height[i]) - bar) * (i - bars.peek() - 1);
+
+				}
+			}
+		}
+		return ans;
+	}
 }
