bbfb2ff58daaf1809b6415f018b689e5bc3d83e2
piyush121 <pc1905@nyu.edu>
2016-11-12 20:46:46 -0500

    sick commit!

---
+++ src/hackerRank/MaximalRectangle.java
@@ -0,0 +1,23 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing
+ * only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 6.
+ *
+ */
+public class MaximalRectangle {
+	public int maximalRectangle(char[][] matrix) { // Working on it...
+        
+    }
+}
