0cb4d9f80bc8abc316c95d33d908ede4c05578cb
piyush121 <pc1905@nyu.edu>
2016-11-18 17:15:07 -0500

    back to algos after mid-term.

--- src/hackerRank/KthSmallestElementInSortedMatrix.java
+++ src/hackerRank/KthSmallestElementInSortedMatrix.java
@@ -3,6 +3,9 @@
  */
 package hackerRank;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
 
@@ -22,8 +25,36 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ n2.
  *
  */
+
+class Tuple {
+	int val;
+	int row;
+	int col;
+	public Tuple(int val, int row, int col) {
+		this.val = val;
+		this.col = col;
+		this.row = row;
+	}
+}
 public class KthSmallestElementInSortedMatrix {
 	 public int kthSmallest(int[][] matrix, int k) {
+		 if(matrix.length == 0)
+			 return -1;
+	        PriorityQueue<Tuple> que = new PriorityQueue<>(new Comparator<Tuple>() {
+	        	public int compare(Tuple t1, Tuple t2) {
+	        		return t1.val - t2.val;
+	        	}
+			});
+	        for(int i = 0 ; i < matrix[0].length; i++) // put 1st row in the que.
+	        	que.offer(new Tuple(matrix[0][i], 0, i));
+	        Tuple curr = null;
+	        while(k > 0) { // extract row and col of the lowest val and put the next item in the same row. 
+	        	curr = que.poll();
+	        	if(curr.row < matrix[0].length - 1)
+	        		que.offer(new Tuple(matrix[curr.row + 1][curr.col], curr.row + 1, curr.col));
+	        	k--;
+	        }
 	        
+	        return curr.val;
 	    }
 }
--- src/hackerRank/LonelyInteger.java
+++ src/hackerRank/LonelyInteger.java
@@ -3,10 +3,25 @@
  */
 package hackerRank;
 
+import java.util.Scanner;
+
 /**
  * @author Piyush Chaudhary
  *
  */
 public class LonelyInteger {
-
+	 private static int lonelyInteger(int[] a) {
+	        for(int i = 1; i < a.length; i++)
+	            a[i] ^= a[i - 1];
+	         return a[a.length - 1];
+	    }
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        int[] a = new int[n];
+	        for (int i = 0; i < n; i++) {
+	            a[i] = in.nextInt();
+	        }
+	        System.out.println(lonelyInteger(a));
+	    }
 }
