dde9ed0876e5ebe4dcb1135fff7b19b6dbc46a70
piyush121 <pc1905@nyu.edu>
2016-11-19 12:27:20 -0500

    done.

---
+++ src/hackerRank/MaximumProductOfWordsLength.java
@@ -0,0 +1,55 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a string array words, find the maximum value of length(word[i]) * length(word[j]) where the 
+ * two words do not share common letters. You may assume that each word will contain only lower case letters. 
+ * If no such two words exist, return 0.
+
+Example 1:
+Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+Return 16
+The two words can be "abcw", "xtfn".
+
+Example 2:
+Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+Return 4
+The two words can be "ab", "cd".
+
+Example 3:
+Given ["a", "aa", "aaa", "aaaa"]
+Return 0
+No such pair of words.
+ *
+ */
+public class MaximumProductOfWordsLength {
+	public int maxProduct(String[] words) {
+        int[][] allWords = new int[words.length][26];
+        
+        for(int i = 0 ; i < words.length; i++) { // Preprocessing.
+            for(char ch : words[i].toCharArray())
+                allWords[i][ch - 'a'] = 1;
+        }
+        
+        int max = 0;
+        for(int i = 0; i < words.length; i++) {
+            for(int j = i + 1; j < words.length; j++){
+                if(compareArr(allWords[i], allWords[j])){ // compare every processd string
+                    max = Math.max(max, words[i].length() * words[j].length());
+                    
+                }
+            }
+        }
+        return max;
+    }
+    
+    public boolean compareArr(int[] arr1, int[] arr2) {
+        
+        for(int i = 0 ; i < 26; i++)
+        	if(arr1[i] == 1 &&  arr2[i] == 1)
+                return false;
+        return true;
+    }
+}
