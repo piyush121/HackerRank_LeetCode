41812160c5c3b61b7127a7dc4c72cf2342bd1d55
piyush121 <pc1905@nyu.edu>
2016-11-25 17:30:43 -0500

    Thanksgiving commit!

--- src/hackerRank/PartitionEqualSubsetSum.java
+++ src/hackerRank/PartitionEqualSubsetSum.java
@@ -28,27 +28,30 @@ Explanation: The array cannot be partitioned into equal sum subsets.
  */
 public class PartitionEqualSubsetSum {
 	public boolean canPartition(int[] nums) {
-        if(nums.length == 0)
-            return false;
-        int sum = 0;
-        for(int num : nums)
-            sum += num;
-        if(sum % 2 != 0)
-            return false;
-        sum /= 2;
-        return dfs(nums, 0, sum);
-    }
+		if (nums.length == 0)
+			return false;
+		int sum = 0;
+		for (int num : nums)
+			sum += num;
+		if (sum % 2 != 0)
+			return false;
+		sum /= 2;
+		return dfs(nums, 0, sum);
+	}
 
-    public boolean dfs(int[] nums, int start, int sum) { // DFS approach. Exponential. Not efficient at all !
-        if(sum == 0)
-            return true;
-        if(start == nums.length)
-            return false;
+	public boolean dfs(int[] nums, int start, int sum) { // DFS approach.
+															// Exponential. Not
+															// efficient at all
+															// !
+		if (sum == 0)
+			return true;
+		if (start == nums.length)
+			return false;
 
-        for(int i = start; i < nums.length; i++) {
-            if(dfs(nums, i + 1, sum - nums[i]))
-                return true;
-        }
-    return false;
-    }
+		for (int i = start; i < nums.length; i++) {
+			if (dfs(nums, i + 1, sum - nums[i]))
+				return true;
+		}
+		return false;
+	}
 }
