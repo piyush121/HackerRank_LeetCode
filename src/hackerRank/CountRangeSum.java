e16ab0ef71669fcaf2d7e1a20de675a8cfbd2d55
piyush121 <pc1905@nyu.edu>
2016-12-08 23:15:26 -0500

    ahh..

--- src/hackerRank/CountRangeSum.java
+++ src/hackerRank/CountRangeSum.java
@@ -17,7 +17,7 @@ The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2,
  *
  */
 public class CountRangeSum { // Hard one !
-	public int countRangeSum(int[] nums, int lower, int upper) { // O(N^2) Brute-Force approach.
+	public int countRangeSum1(int[] nums, int lower, int upper) { // O(N^2) Brute-Force approach.
 		int n = nums.length;
 		long[] sums = new long[n + 1];
 		for (int i = 0; i < n; ++i)
@@ -28,5 +28,35 @@ public class CountRangeSum { // Hard one !
 				if (sums[j] - sums[i] >= lower && sums[j] - sums[i] <= upper)
 					ans++;
 		return ans;
+	}
+	
+	public int countRangeSum(int[] nums, int lower, int upper) { // D&C based solution. Pretty tough!
+		int n = nums.length;
+		int[] sums = new int[n + 1];
+		for (int i = 0; i < n; ++i)
+			sums[i + 1] = sums[i] + nums[i];
+
+		return countRangeMerge(sums, 0, n, lower, upper);
+	}
+
+	public int countRangeMerge(int[] sums, int start, int end, int lower, int upper) {
+		if (start >= end)
+			return 0;
+		int mid = (start + end) / 2;
+		int count = countRangeMerge(sums, start, mid - 1, lower, upper) + countRangeMerge(sums, mid, end, lower, upper);
+		int j = mid, k = mid, t = mid, r = 0;
+		int cache[] = new int[end - start];
+		for (int i = start; i < mid; i++) {
+
+			while (k <= end && sums[k] - sums[i] < lower) 
+				k++;
+			while (j <= end && sums[j] - sums[i] <= upper)
+				j++;
+			while (t <= end && sums[t] < sums[i])
+				cache[r++] = sums[t++];
+			count += j - k;// because you can take all the index from j to i and make them within the given bounds.
+		}
+		System.arraycopy(cache, 0, sums, start, t - start);
+		return count;
 	}
 }
