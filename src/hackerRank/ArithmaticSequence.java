bf3fcdc8693766b6c84986cd5a1bb929572e3ded
piyus <piyus@ADMINRG-LHA1GL5>
2017-01-11 16:56:55 +0530

    coming back on speed.

--- src/hackerRank/ArithmaticSequence.java
+++ src/hackerRank/ArithmaticSequence.java
@@ -42,4 +42,18 @@ public class ArithmaticSequence {
 		}
 		return res;
 	}
+	
+	 public int numberOfArithmeticSlices1(int[] A) { // O(1) space and O(n) time. We just need the previous value.
+	        int curr = 0;
+	        int res = 0;
+	        for(int i = 2; i < A.length; i++) {
+	            if(A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+	                curr++;
+	                res += curr;
+	            }
+	            else
+	                curr = 0;
+	        }
+	        return res;
+	    }
 }
