06fb1ca36b29a174332fa6d36b97ff088247a990
Piyush Chaudhary <pc1905@nyu.edu>
2017-02-24 18:01:41 -0500

    work...

---
+++ src/hackerRank/MaximumConsecutiveOnes_II.java
@@ -0,0 +1,38 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0.
+
+Example 1:
+Input: [1,0,1,1,0]
+Output: 4
+Explanation: Flip the first zero will get the the maximum number of consecutive 1s.
+    After flipping, the maximum number of consecutive 1s is 4.
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000
+Follow up:
+What if the input numbers come in one by one as an infinite stream? In other words, you can't store all numbers coming from the stream as it's too large to hold in memory. Could you solve it efficiently?
+ *
+ */
+public class MaximumConsecutiveOnes_II {
+	public int findMaxConsecutiveOnes(int[] nums) {
+		if (nums.length == 0)
+			return 0;
+		int zero = 0;
+		int last = -1; //Index of last zero.
+		int res = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				zero = last + 1; // set zero to last seen zero + 1 because 1 zero is allowed.
+				last = i;
+			}
+			res = Math.max(res, i - zero + 1);
+		}
+		return res;
+	}
+}
