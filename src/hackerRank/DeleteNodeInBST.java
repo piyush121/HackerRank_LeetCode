88c195645615b9a09c9680d8ca4a5a4dbd3b3e0f
piyus <piyus@ADMINRG-LHA1GL5>
2017-03-04 11:21:34 -0500

    work...

---
+++ src/hackerRank/DeleteNodeInBST.java
@@ -0,0 +1,73 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+/**
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+
+Example:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+ *
+ */
+public class DeleteNodeInBST {
+	public TreeNode deleteNode(TreeNode root, int key) {
+		if (root == null)
+			return null;
+		if (key < root.val)
+			root.left = deleteNode(root.left, key);
+		else if (key > root.val)
+			root.right = deleteNode(root.right, key);
+		else { // The base case where we want to be.
+			if (root.left == null)
+				return root.right;
+			if (root.right == null)
+				return root.left;
+			TreeNode minNode = findMinNode(root.right);
+			if (minNode != null) {
+				root.val = minNode.val;
+				root.right = deleteNode(root.right, root.val);
+			}
+		}
+
+		return root;
+	}
+
+	public TreeNode findMinNode(TreeNode root) {
+		while (root.left != null)
+			root = root.left;
+		return root;
+	}
+}
