a36001c5953428d2cb2a993bb02ce0d3013615c3
piyus <piyus@ADMINRG-LHA1GL5>
2017-03-23 21:53:31 -0400

    Commit hard!

--- src/JavaTest/BoxPrinter.java
+++ src/JavaTest/BoxPrinter.java
@@ -9,12 +9,15 @@ import java.util.*;
  * This is a test for generic class.
  *
  */
-public class BoxPrinter<T> {
-	private T var1, var2;
+public class BoxPrinter<T> implements Comparable<BoxPrinter<T>> {
+	public int var1;
+	public T var2;
 	public BoxPrinter( int x, T y) {
-		//this.var1 = x;
+		this.var1 = x;
 		this.var2 = y;
 	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BoxPrinter<String> box = new BoxPrinter<>(10, "bye");
@@ -27,6 +30,54 @@ public class BoxPrinter<T> {
 			itr.next();
 			itr.remove();
 		}
+		Integer a = 500;
+		Integer b = 500;
+		System.out.println(a == b);
+		Collections.sort(list, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer i1, Integer i2) {
+				return 0;
+			}
+			
+		});
+		//Map<String, String> map = new Hashtable<>();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + var1;
+		result = prime * result + ((var2 == null) ? 0 : var2.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BoxPrinter other = (BoxPrinter) obj;
+		if (var1 != other.var1)
+			return false;
+		if (var2 == null) {
+			if (other.var2 != null)
+				return false;
+		} else if (!var2.equals(other.var2))
+			return false;
+		return true;
+	}
+
+
+	public int compareTo(BoxPrinter<T> o) {
+		// TODO Auto-generated method stub
+		return this.var1 - o.var1;
+	}
+	
+	
 
 }
