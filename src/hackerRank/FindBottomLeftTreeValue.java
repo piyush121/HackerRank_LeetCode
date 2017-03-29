02d7d06e2c0c34250bedd3f641250a14060f1e2f
Piyush <pc1905@nyu.edu>
2017-03-29 18:16:09 -0400

    work...

---
+++ src/hackerRank/FindBottomLeftTreeValue.java
@@ -0,0 +1,56 @@
+/**
+ * @author Piyush Chaudhary
+ */
+package hackerRank;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+
+Example 1:
+Input:
+
+    2
+   / \
+  1   3
+
+Output:
+1
+Example 2: 
+Input:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   5   6
+       /
+      7
+
+Output:
+7
+ *
+ */
+public class FindBottomLeftTreeValue {
+	public int findBottomLeftValue(TreeNode root) { // BFS. O(N) time and space.
+		int res = 0;
+		Queue<TreeNode> nodes = new LinkedList<>();
+		nodes.offer(root);
+
+		while (!nodes.isEmpty()) {
+			int size = nodes.size();
+			TreeNode curr = nodes.peek();
+			res = curr.val;
+			for (int i = 0; i < size; i++) {
+				TreeNode parent = nodes.poll();
+				if (parent.left != null)
+					nodes.offer(parent.left);
+				if (parent.right != null)
+					nodes.offer(parent.right);
+			}
+		}
+		return res;
+	}
+}
